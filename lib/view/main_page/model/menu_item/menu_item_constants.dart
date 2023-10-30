192be35f2139a29137a202b82f8c54d341ec142d
BurakTalhaY <127734030+BurakTalhaY@users.noreply.github.com>
2023-10-31 00:28:13 +0300

    versiyon 34 ve yeni menü yetki açıldı.

--- lib/view/main_page/model/menu_item/menu_item_constants.dart
+++ lib/view/main_page/model/menu_item/menu_item_constants.dart
@@ -200,7 +200,7 @@ class MenuItemConstants {
           ],
         ),
       ],
-    ).isDebug(),
+    ),
 
     //* Sayım
     //*
@@ -218,7 +218,7 @@ class MenuItemConstants {
         GridItemModel.item(name: "sevkiyat_SatisIrsaliyesi", title: "Satış İrsaliyesi", route: "/mainPage/sevkiyatSatisIrsaliyesi"),
         GridItemModel.item(name: "sevkemri_MalToplama", title: "Mal Toplama"),
       ],
-    ).isDebug(),
+    )),
 
     //* Sipariş
     //*
