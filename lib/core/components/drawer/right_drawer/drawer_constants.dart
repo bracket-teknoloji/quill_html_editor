ef1b25cf801340dfabc0bc73839570942e7ceeea
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-11-27 12:28:07 +0300

    Sağ Drawer şirket bilgileri hatası giderildi.

--- lib/core/components/drawer/right_drawer/drawer_constants.dart
+++ lib/core/components/drawer/right_drawer/drawer_constants.dart
@@ -6,39 +6,39 @@ import "../../../init/cache/cache_manager.dart";
 import "drawer_model.dart";
 
 class DrawerMenuItems {
-  static final List<DrawerModel> items = [
-    DrawerModel(
-      title: "${CacheManager.getVeriTabani()["Şirket"]} (${CacheManager.getVeriTabani()["Şube"]})",
-      iconWidget: Icons.storage_outlined,
-    ),
-    DrawerModel(
-      title: CacheManager.getIsletmeSube["İşletme"],
-      iconWidget: Icons.home_outlined,
-    ),
-    // DrawerModel(
-    //   title: "${CacheManager.getAccounts(AccountModel.instance.uyeEmail ?? "")?.wsWan}",
-    //   iconWidget: Icons.router_outlined,
-    // ),
-    DrawerModel(
-      title: "${CacheManager.getVeriTabani()["Şube"]} - ${CacheManager.getIsletmeSube["Şube"]}",
-      iconWidget: Icons.location_on_outlined,
-    ),
-    DrawerModel(
-      title: "v${AppInfoModel.instance.version}",
-      iconWidget: Icons.route_outlined,
-    ),
-    // DrawerModel(
-    //   title: AccountModel.instance.localIp,
-    //   iconWidget: Icons.link_outlined,
-    // ),
-    DrawerModel(
-      title: "Sürüm Yenilikleri",
-      iconWidget: Icons.new_releases_outlined,
-      onTap: () {
-        //close Drawer
-        Get.back();
-        return Get.toNamed("/mainPage/surumYenilikleri");
-      },
-    ),
-  ];
+  static List<DrawerModel> get items => [
+        DrawerModel(
+          title: "${CacheManager.getVeriTabani()["Şirket"]} (${CacheManager.getVeriTabani()["Şube"]})",
+          iconWidget: Icons.storage_outlined,
+        ),
+        DrawerModel(
+          title: CacheManager.getIsletmeSube["İşletme"],
+          iconWidget: Icons.home_outlined,
+        ),
+        // DrawerModel(
+        //   title: "${CacheManager.getAccounts(AccountModel.instance.uyeEmail ?? "")?.wsWan}",
+        //   iconWidget: Icons.router_outlined,
+        // ),
+        DrawerModel(
+          title: "${CacheManager.getVeriTabani()["Şube"]} - ${CacheManager.getIsletmeSube["Şube"]}",
+          iconWidget: Icons.location_on_outlined,
+        ),
+        DrawerModel(
+          title: "v${AppInfoModel.instance.version}",
+          iconWidget: Icons.route_outlined,
+        ),
+        // DrawerModel(
+        //   title: AccountModel.instance.localIp,
+        //   iconWidget: Icons.link_outlined,
+        // ),
+        DrawerModel(
+          title: "Sürüm Yenilikleri",
+          iconWidget: Icons.new_releases_outlined,
+          onTap: () {
+            //close Drawer
+            Get.back();
+            return Get.toNamed("/mainPage/surumYenilikleri");
+          },
+        ),
+      ];
 }
