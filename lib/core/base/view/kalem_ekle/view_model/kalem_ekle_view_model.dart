2164ca900ab41ac18d173680dc36b4712b112327
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-06 17:02:05 +0300

    Add imports and mixins to KalemEkleViewModel

--- lib/core/base/view/kalem_ekle/view_model/kalem_ekle_view_model.dart
+++ lib/core/base/view/kalem_ekle/view_model/kalem_ekle_view_model.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
 import "package:mobx/mobx.dart";
+import "package:picker/core/base/view/stok_rehberi/model/stok_rehberi_request_model.dart";
+import "package:picker/core/base/view_model/mobx_network_mixin.dart";
+import "package:picker/core/init/network/login/api_urls.dart";
 
 import "../../../../../view/main_page/alt_sayfalar/siparis/base_siparis_edit/model/base_siparis_edit_model.dart";
 import "../../../../../view/main_page/alt_sayfalar/stok/stok_liste/model/stok_listesi_model.dart";
@@ -11,7 +14,7 @@ part "kalem_ekle_view_model.g.dart";
 
 class KalemEkleViewModel = _KalemEkleViewModelBase with _$KalemEkleViewModel;
 
-abstract class _KalemEkleViewModelBase with Store {
+abstract class _KalemEkleViewModelBase with Store, MobxNetworkMixin {
   @observable
   bool showDovizBilgileri = true;
 
@@ -30,7 +33,7 @@ abstract class _KalemEkleViewModelBase with Store {
   }
 
   @computed
-  bool get dovizliMi => model?.dovizliMi ?? false;
+  bool get dovizliMi => (model?.dovizliMi ?? false) || kalemModel.dovizliMi;
 
   @computed
   String get dovizAdi => StaticVariables.instance.isMusteriSiparisleri ? kalemModel.stokSatDovizAdi ?? "" : kalemModel.stokAlisDovizAdi ?? "";
@@ -54,48 +57,70 @@ abstract class _KalemEkleViewModelBase with Store {
   @computed
   bool get koliMi => (model?.koliMi ?? false) || (kalemModel.koliMi ?? false) || (kalemModel.kalemList?.isNotEmpty ?? false);
 
+  
   @action
   void setMuhasebeKodu(String? value) => kalemModel = kalemModel.copyWith(muhasebeKodu: value);
+  
   @action
   void setKalemModel(KalemModel? value) => kalemModel = value ?? KalemModel();
+  
   @action
   void setDovizAdi(String? value) => kalemModel = StaticVariables.instance.isMusteriSiparisleri ? kalemModel.copyWith(stokSatDovizAdi: value) : kalemModel.copyWith(stokAlisDovizAdi: value);
+  
   @action
   void setYapKod(String? value) => kalemModel = kalemModel.copyWith(yapkod: value);
+  
   @action
   void setDovizFiyati(double? value) => kalemModel = kalemModel.copyWith(dovizFiyati: value);
+  
   @action
   void setOlcuBirimi(MapEntry<String, int>? value) => kalemModel = kalemModel.copyWith(olcuBirimKodu: value?.value, olcuBirimAdi: value?.key);
+  
   @action
   void setFiyat(double? value) => kalemModel = kalemModel.copyWith(satisFiyati: value);
+  
   @action
   void setProjeKodu(String? value) => kalemModel = kalemModel.copyWith(projeKodu: value);
+  
   @action
   void setDepoKodu(int? value) => kalemModel = kalemModel.copyWith(depoKodu: value);
+  
   @action
   void setKosul(String? value) => kalemModel = kalemModel.copyWith(kosulKodu: value);
+  
   @action
   void setMiktar(int? value) => kalemModel = kalemModel.copyWith(miktar: value?.toDouble());
+  
   @action
   void setBrutFiyat(double? value) => kalemModel = kalemModel.copyWith(brutFiyat: value);
+  
   @action
   void setMFTutari(double? value) => kalemModel = kalemModel.copyWith(malFazlasiMiktar: value);
+  
   @action
   void setKdvOrani(double? value) => kalemModel = kalemModel.copyWith(kdvOrani: value);
+  
   @action
   void setIskonto1(double? value) => kalemModel = kalemModel.copyWith(iskonto1: value != 0 ? value : null);
+  
   @action
   void setIskonto2(double? value) => kalemModel = kalemModel.copyWith(iskonto2: value != 0 ? value : null);
+  
   @action
   void setIskonto3(double? value) => kalemModel = kalemModel.copyWith(iskonto3: value != 0 ? value : null);
+  
   @action
   void setIskonto4(double? value) => kalemModel = kalemModel.copyWith(iskonto4: value != 0 ? value : null);
+  
   @action
   void setIskonto5(double? value) => kalemModel = kalemModel.copyWith(iskonto5: value != 0 ? value : null);
+  
   @action
   void setIskonto6(double? value) => kalemModel = kalemModel.copyWith(iskonto6: value != 0 ? value : null);
+  
   @action
   void setKalemList(List<KalemModel>? list) => kalemModel = kalemModel.copyWith(kalemList: list);
+  
   @action
   void setIskonto1OranMi(bool? value) => kalemModel = kalemModel.copyWith(
         iskonto1OranMi: value,
@@ -167,6 +192,13 @@ abstract class _KalemEkleViewModelBase with Store {
         malfazIskAdedi: (kalemModel.malFazlasiMiktar ?? 0) - 1,
       );
       controller.text = (kalemModel.malFazlasiMiktar ?? 0).toIntIfDouble.toString();
+    }
+  }
+
+  Future<void> getData(StokRehberiRequestModel model) async {
+    final result = await networkManager.dioPost(path: ApiUrls.getStoklar, bodyModel: StokListesiModel(), showLoading: true, data: model.toJson());
+    if (result.success ?? false) {
+      setModel(result.data.first);
     }
   }
 }
