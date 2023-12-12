fc66a327f0bc5d1dffc936647f2a43e5f7848692
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-12 16:18:08 +0300

    Add e_irsaliye_ek_bilgiler_view_model.dart and e_irsaliye_ek_bilgiler_view_model.g.dart

---
+++ lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.dart
@@ -0,0 +1,7 @@
+import "package:mobx/mobx.dart";
+
+part "e_irsaliye_ek_bilgiler_view_model.g.dart";
+
+class EIrsaliyeEkBilgilerViewModel = _EIrsaliyeEkBilgilerViewModelBase with _$EIrsaliyeEkBilgilerViewModel;
+
+abstract class _EIrsaliyeEkBilgilerViewModelBase with Store {}
---
+++ lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'e_irsaliye_ek_bilgiler_view_model.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$EIrsaliyeEkBilgilerViewModel
+    on _EIrsaliyeEkBilgilerViewModelBase, Store {
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
