3de1e74402fe0c07951441026c7a2e896c16c19b
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-12 17:53:01 +0300

    Add view model for e_irsaliye_ek_bilgiler

--- lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.dart
+++ lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.dart
@@ -1,7 +1,95 @@
+import "package:flutter/material.dart";
 import "package:mobx/mobx.dart";
+import "package:picker/core/base/view/e_irsaliye_ek_bilgiler/model/e_irsaliye_bilgi_model.dart";
+import "package:picker/view/main_page/alt_sayfalar/cari/cari_listesi/model/cari_listesi_model.dart";
 
 part "e_irsaliye_ek_bilgiler_view_model.g.dart";
 
 class EIrsaliyeEkBilgilerViewModel = _EIrsaliyeEkBilgilerViewModelBase with _$EIrsaliyeEkBilgilerViewModel;
 
-abstract class _EIrsaliyeEkBilgilerViewModelBase with Store {}
+abstract class _EIrsaliyeEkBilgilerViewModelBase with Store {
+  @observable
+  EIrsaliyeBilgiModel model = EIrsaliyeBilgiModel();
+
+  @action
+  void setModel(EIrsaliyeBilgiModel model) => this.model = model;
+
+  @action
+  void setPlaka(String? plaka) => model = model.copyWith(plaka: plaka);
+
+  @action
+  void setSevkTarihi(DateTime? sevkTarihi) => model = model.copyWith(sevktar: sevkTarihi);
+
+  @action
+  void setSevkSaati(TimeOfDay? sevkSaati) => model = model.copyWith(sevktar: model.sevktar?.copyWith(hour: sevkSaati!.hour, minute: sevkSaati.minute));
+
+  @action
+  void setUnvan(CariListesiModel? model) => this.model = this.model.copyWith(
+        tasiyiciUnvan: model?.cariAdi,
+        tasiyiciIl: model?.cariIl,
+        tasiyiciIlce: model?.cariIlce,
+        tasiyiciUlke: model?.ulkeAdi,
+        tasiyiciPostakodu: model?.postakodu,
+        tasiyiciVkn: model?.vergiNumarasi,);
+
+  @action
+  void setVergiNo(String? vergiNo) => model = model.copyWith(tasiyiciVkn: vergiNo);
+
+  @action
+  void setIl(String? il) => model = model.copyWith(tasiyiciIl: il);
+
+  @action
+  void setIlce(String? ilce) => model = model.copyWith(tasiyiciIlce: ilce);
+
+  @action
+  void setUlke(String? ulke) => model = model.copyWith(tasiyiciUlke: ulke);
+
+  @action
+  void setPostaKodu(String? postaKodu) => model = model.copyWith(tasiyiciPostakodu: postaKodu);
+
+  @action
+  void setSofor1Adi(String? sofor1Adi) => model = model.copyWith(sofor1Adi: sofor1Adi);
+
+  @action
+  void setSofor1Soyadi(String? sofor1Soyadi) => model = model.copyWith(sofor1Soyadi: sofor1Soyadi);
+
+  @action
+  void setSofor1Aciklama(String? sofor1Aciklama) => model = model.copyWith(sofor1Aciklama: sofor1Aciklama);
+
+  @action
+  void setSofor1TcKimlikNo(String? sofor1TcKimlikNo) => model = model.copyWith(sofor1KimlikNo: sofor1TcKimlikNo);
+
+  @action
+  void setSofor2Adi(String? sofor2Adi) => model = model.copyWith(sofor2Adi: sofor2Adi);
+
+  @action
+  void setSofor2Soyadi(String? sofor2Soyadi) => model = model.copyWith(sofor2Soyadi: sofor2Soyadi);
+
+  @action
+  void setSofor2Aciklama(String? sofor2Aciklama) => model = model.copyWith(sofor2Aciklama: sofor2Aciklama);
+
+  @action
+  void setSofor2TcKimlikNo(String? sofor2TcKimlikNo) => model = model.copyWith(sofor2KimlikNo: sofor2TcKimlikNo);
+
+  @action
+  void setSofor3Adi(String? sofor3Adi) => model = model.copyWith(sofor3Adi: sofor3Adi);
+
+  @action
+  void setSofor3Soyadi(String? sofor3Soyadi) => model = model.copyWith(sofor3Soyadi: sofor3Soyadi);
+
+  @action
+  void setSofor3Aciklama(String? sofor3Aciklama) => model = model.copyWith(sofor3Aciklama: sofor3Aciklama);
+
+  @action
+  void setSofor3TcKimlikNo(String? sofor3TcKimlikNo) => model = model.copyWith(sofor3KimlikNo: sofor3TcKimlikNo);
+
+  @action
+  void setDorsePlaka1(String? dorsePlaka1) => model = model.copyWith(dorsePlaka1: dorsePlaka1);
+
+  @action
+  void setDorsePlaka2(String? dorsePlaka2) => model = model.copyWith(dorsePlaka2: dorsePlaka2);
+
+  @action
+  void setDorsePlaka3(String? dorsePlaka3) => model = model.copyWith(dorsePlaka3: dorsePlaka3);
+
+}
--- lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.g.dart
+++ lib/core/base/view/e_irsaliye_ek_bilgiler/view_model/e_irsaliye_ek_bilgiler_view_model.g.dart
@@ -10,10 +10,336 @@ part of 'e_irsaliye_ek_bilgiler_view_model.dart';
 
 mixin _$EIrsaliyeEkBilgilerViewModel
     on _EIrsaliyeEkBilgilerViewModelBase, Store {
+  late final _$modelAtom =
+      Atom(name: '_EIrsaliyeEkBilgilerViewModelBase.model', context: context);
+
+  @override
+  EIrsaliyeBilgiModel get model {
+    _$modelAtom.reportRead();
+    return super.model;
+  }
+
+  @override
+  set model(EIrsaliyeBilgiModel value) {
+    _$modelAtom.reportWrite(value, super.model, () {
+      super.model = value;
+    });
+  }
+
+  late final _$_EIrsaliyeEkBilgilerViewModelBaseActionController =
+      ActionController(
+          name: '_EIrsaliyeEkBilgilerViewModelBase', context: context);
+
+  @override
+  void setModel(EIrsaliyeBilgiModel model) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setModel');
+    try {
+      return super.setModel(model);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPlaka(String? plaka) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setPlaka');
+    try {
+      return super.setPlaka(plaka);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSevkTarihi(DateTime? sevkTarihi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSevkTarihi');
+    try {
+      return super.setSevkTarihi(sevkTarihi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSevkSaati(TimeOfDay? sevkSaati) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSevkSaati');
+    try {
+      return super.setSevkSaati(sevkSaati);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setUnvan(CariListesiModel? model) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setUnvan');
+    try {
+      return super.setUnvan(model);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setVergiNo(String? vergiNo) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setVergiNo');
+    try {
+      return super.setVergiNo(vergiNo);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setIl(String? il) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setIl');
+    try {
+      return super.setIl(il);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setIlce(String? ilce) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setIlce');
+    try {
+      return super.setIlce(ilce);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setUlke(String? ulke) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setUlke');
+    try {
+      return super.setUlke(ulke);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPostaKodu(String? postaKodu) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setPostaKodu');
+    try {
+      return super.setPostaKodu(postaKodu);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor1Adi(String? sofor1Adi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor1Adi');
+    try {
+      return super.setSofor1Adi(sofor1Adi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor1Soyadi(String? sofor1Soyadi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor1Soyadi');
+    try {
+      return super.setSofor1Soyadi(sofor1Soyadi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor1Aciklama(String? sofor1Aciklama) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor1Aciklama');
+    try {
+      return super.setSofor1Aciklama(sofor1Aciklama);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor1TcKimlikNo(String? sofor1TcKimlikNo) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor1TcKimlikNo');
+    try {
+      return super.setSofor1TcKimlikNo(sofor1TcKimlikNo);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor2Adi(String? sofor2Adi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor2Adi');
+    try {
+      return super.setSofor2Adi(sofor2Adi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor2Soyadi(String? sofor2Soyadi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor2Soyadi');
+    try {
+      return super.setSofor2Soyadi(sofor2Soyadi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor2Aciklama(String? sofor2Aciklama) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor2Aciklama');
+    try {
+      return super.setSofor2Aciklama(sofor2Aciklama);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor2TcKimlikNo(String? sofor2TcKimlikNo) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor2TcKimlikNo');
+    try {
+      return super.setSofor2TcKimlikNo(sofor2TcKimlikNo);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor3Adi(String? sofor3Adi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor3Adi');
+    try {
+      return super.setSofor3Adi(sofor3Adi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor3Soyadi(String? sofor3Soyadi) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor3Soyadi');
+    try {
+      return super.setSofor3Soyadi(sofor3Soyadi);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor3Aciklama(String? sofor3Aciklama) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor3Aciklama');
+    try {
+      return super.setSofor3Aciklama(sofor3Aciklama);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setSofor3TcKimlikNo(String? sofor3TcKimlikNo) {
+    final _$actionInfo =
+        _$_EIrsaliyeEkBilgilerViewModelBaseActionController.startAction(
+            name: '_EIrsaliyeEkBilgilerViewModelBase.setSofor3TcKimlikNo');
+    try {
+      return super.setSofor3TcKimlikNo(sofor3TcKimlikNo);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setDorsePlaka1(String? dorsePlaka1) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setDorsePlaka1');
+    try {
+      return super.setDorsePlaka1(dorsePlaka1);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setDorsePlaka2(String? dorsePlaka2) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setDorsePlaka2');
+    try {
+      return super.setDorsePlaka2(dorsePlaka2);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setDorsePlaka3(String? dorsePlaka3) {
+    final _$actionInfo = _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+        .startAction(name: '_EIrsaliyeEkBilgilerViewModelBase.setDorsePlaka3');
+    try {
+      return super.setDorsePlaka3(dorsePlaka3);
+    } finally {
+      _$_EIrsaliyeEkBilgilerViewModelBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+model: ${model}
     ''';
   }
 }
