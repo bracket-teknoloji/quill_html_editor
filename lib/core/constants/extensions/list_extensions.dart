fbb8aad5508ef17a9b43e8c538ea20b39e7828d4
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-13 10:42:51 +0300

    Add account model import and update filteredDizaynList method

--- lib/core/constants/extensions/list_extensions.dart
+++ lib/core/constants/extensions/list_extensions.dart
@@ -4,6 +4,7 @@ import "package:get/get.dart";
 import "package:picker/core/constants/enum/edit_tipi_enum.dart";
 import "package:picker/core/constants/ui_helper/ui_helper.dart";
 import "package:picker/core/init/cache/cache_manager.dart";
+import "package:picker/view/add_company/model/account_model.dart";
 import "package:picker/view/main_page/model/param_model.dart";
 import "package:picker/view/main_page/model/user_model/profil_yetki_model.dart";
 
@@ -25,7 +26,6 @@ extension ListExtension<T> on List<T> {
   List<T> get nullCheck => where((element) => element != null).toList().cast<T>();
 }
 
-
 // an extension on a list or ObservableList to check is empty or null
 extension ListExtension3<T> on List<T>? {
   bool get isEmptyOrNull => this == null || this!.isEmpty;
@@ -41,7 +41,6 @@ extension ListExtension4<T> on List? {
   List<T> cast(T model) => this!.map((e) => e as T).toList().cast<T>();
 }
 
-
 extension ListGenericNullCheck<T> on List<T?> {
   List<T> get nullCheckWithGeneric => whereType<T>().toList();
 }
@@ -49,21 +48,24 @@ extension ListGenericNullCheck<T> on List<T?> {
 extension DizaynListExtensions on List<NetFectDizaynList> {
   List<NetFectDizaynList> filteredDizaynList(EditTipiEnum? editTipiEnum) {
     final ProfilYetkiModel profilYetkiModel = CacheManager.getAnaVeri?.userModel?.profilYetki ?? ProfilYetkiModel();
+    if (AccountModel.instance.admin == "E") {
+      return this;
+    }
     switch (editTipiEnum) {
       case EditTipiEnum.satisTeklifi:
-        return where((element) => profilYetkiModel.yazdirmaDizaynSatisIrs?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynSatisIrs?.any((element2) => element.aktifMi) ?? false).toList();
       case EditTipiEnum.satisIrsaliye:
-        return where((element) => profilYetkiModel.yazdirmaDizaynSatisIrs?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynSatisIrs?.any((element2) => element.aktifMi) ?? false).toList();
       case EditTipiEnum.satisFatura:
-        return where((element) => profilYetkiModel.yazdirmaDizaynSatisFat?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynSatisFat?.any((element2) => element.aktifMi) ?? false).toList();
       case EditTipiEnum.alisIrsaliye:
-        return where((element) => profilYetkiModel.yazdirmaDizaynAlisIrs?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynAlisIrs?.any((element2) => element.aktifMi) ?? false).toList();
       case EditTipiEnum.alisFatura:
-        return where((element) => profilYetkiModel.yazdirmaDizaynAlisFat?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynAlisFat?.any((element2) => element.aktifMi) ?? false).toList();
       case EditTipiEnum.satisTalebi:
-        return where((element) => profilYetkiModel.yazdirmaDizaynSatisTalebi?.any((element2) => element.id == element2) ?? false).toList();
+        return where((element) => profilYetkiModel.yazdirmaDizaynSatisTalebi?.any((element2) => element.aktifMi) ?? false).toList();
       // case EditTipiEnum.alis:
-      //   return where((element) => profilYetkiModel.yazdirmaDizaynAli?.any((element2) => element.id == element2) ??false ).toList();
+      //   return where((element) => profilYetkiModel.yazdirmaDizaynAli?.any((element2) => element.aktifMi) ??false ).toList();
       default:
         return this;
     }
