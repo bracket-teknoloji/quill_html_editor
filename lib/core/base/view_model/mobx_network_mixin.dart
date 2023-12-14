74555ac15e8561c5e595624f4c490015903a7043
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-14 17:33:21 +0300

    Add import for DialogManager in MobxNetworkMixin

--- lib/core/base/view_model/mobx_network_mixin.dart
+++ lib/core/base/view_model/mobx_network_mixin.dart
@@ -1,3 +1,5 @@
+import "package:picker/core/components/dialog/dialog_manager.dart";
+
 import "../../../view/main_page/model/param_model.dart";
 import "../../init/cache/cache_manager.dart";
 import "../../init/network/network_manager.dart";
@@ -5,4 +7,5 @@ import "../../init/network/network_manager.dart";
 mixin MobxNetworkMixin {
   NetworkManager networkManager = NetworkManager();
   ParamModel parametreModel = CacheManager.getAnaVeri?.paramModel ?? ParamModel();
+  DialogManager dialogManager = DialogManager();
 }
