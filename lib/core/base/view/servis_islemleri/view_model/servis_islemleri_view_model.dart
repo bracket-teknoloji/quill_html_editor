0934ebb0d28536e99a354fa745148017af66826e
BurakTalhaY <burak.yilmaz@bracket.com.tr>
2023-12-15 10:25:11 +0300

    Remove unused imports and refactor ServisIslemleriViewModel class

--- lib/core/base/view/servis_islemleri/view_model/servis_islemleri_view_model.dart
+++ lib/core/base/view/servis_islemleri/view_model/servis_islemleri_view_model.dart
@@ -1,8 +1,6 @@
-import "package:flutter/material.dart";
 import "package:mobx/mobx.dart";
 import "package:picker/core/base/view/servis_islemleri/model/servis_info_model.dart";
 import "package:picker/core/base/view_model/mobx_network_mixin.dart";
-import "package:picker/core/components/dialog/bottom_sheet/model/bottom_sheet_model.dart";
 import "package:picker/core/init/cache/cache_manager.dart";
 import "package:picker/core/init/network/login/api_urls.dart";
 
@@ -11,33 +9,6 @@ part "servis_islemleri_view_model.g.dart";
 class ServisIslemleriViewModel = _ServisIslemleriViewModelBase with _$ServisIslemleriViewModel;
 
 abstract class _ServisIslemleriViewModelBase with Store, MobxNetworkMixin {
-  // @action
-  // Future<void>
-  List<BottomSheetModel> get itemList => [
-        BottomSheetModel(
-          title: "Web Servisleri Çalışıyor Mu?",
-          description: "Web servislerinin çalışıp çalışmadığını kontrol eder.",
-          iconWidget: Icons.travel_explore_outlined,
-          onTap: webServisleriCalistir,
-        ),
-        BottomSheetModel(
-          title: "Servisi Güncelle",
-          description: "Servisi güncellemek için kullanılır.",
-          iconWidget: Icons.sync_alt_outlined,
-        ),
-        BottomSheetModel(
-          title: "Web Servisi Yeniden Başlat",
-          description: "Web servisini yeniden başlatmak için kullanılır.",
-          iconWidget: Icons.restart_alt_outlined,
-        ),
-        BottomSheetModel(
-          title: "DB Update",
-          description: "SQL veritabanını güncellemek için kullanılır.",
-          iconWidget: Icons.system_update_alt_outlined,
-          onTap: dbUpdate,
-        ),
-      ];
-
   @action
   Future<void> webServisleriCalistir() async {
     final result = await networkManager.dioGet(
@@ -45,7 +16,7 @@ abstract class _ServisIslemleriViewModelBase with Store, MobxNetworkMixin {
       bodyModel: ServisInfoModel(),
     );
     if (result.data.first is ServisInfoModel) {
-      dialogManager.showInfoDialog(result.data.first.getInfo ?? "Servisler Çalışıyor");
+      dialogManager.showSuccesDialog(result.data.first.getInfo ?? "Servisler Çalışıyor");
     }
   }
 
