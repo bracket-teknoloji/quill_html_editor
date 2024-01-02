72b9cf47a2a2236df0c1a7fd30b8640a726042ca
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-02 11:26:43 +0300

    Fix import statements and update code formatting

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1,7 +1,8 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'intl/messages_all.dart';
+import "package:flutter/material.dart";
+import "package:intl/intl.dart";
+
+import "intl/messages_all.dart";
 
 // **************************************************************************
 // Generator: Flutter Intl IDE plugin
@@ -18,17 +19,17 @@ class S {
   static S? _current;
 
   static S get current {
-    assert(_current != null,
-        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    assert(
+      _current != null,
+      "No instance of S was loaded. Try to initialize the S delegate before accessing S.current.",
+    );
     return _current!;
   }
 
   static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
 
   static Future<S> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false)
-        ? locale.languageCode
-        : locale.toString();
+    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
     final localeName = Intl.canonicalizedLocale(name);
     return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
@@ -41,24 +42,22 @@ class S {
 
   static S of(BuildContext context) {
     final instance = S.maybeOf(context);
-    assert(instance != null,
-        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    assert(
+      instance != null,
+      "No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?",
+    );
     return instance!;
   }
 
-  static S? maybeOf(BuildContext context) {
-    return Localizations.of<S>(context, S);
-  }
+  static S? maybeOf(BuildContext context) => Localizations.of<S>(context, S);
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   const AppLocalizationDelegate();
 
-  List<Locale> get supportedLocales {
-    return const <Locale>[
-      Locale.fromSubtags(languageCode: 'en'),
-    ];
-  }
+  List<Locale> get supportedLocales => const <Locale>[
+        Locale.fromSubtags(languageCode: "en"),
+      ];
 
   @override
   bool isSupported(Locale locale) => _isSupported(locale);
