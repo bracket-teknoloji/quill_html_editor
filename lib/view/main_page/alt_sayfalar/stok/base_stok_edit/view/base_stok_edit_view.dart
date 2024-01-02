ac5c0c89adf6a321858a204c59fe3621cfe38c10
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-02 17:58:55 +0300

    Refactor saveStokModel properties

--- lib/view/main_page/alt_sayfalar/stok/base_stok_edit/view/base_stok_edit_view.dart
+++ lib/view/main_page/alt_sayfalar/stok/base_stok_edit/view/base_stok_edit_view.dart
@@ -117,43 +117,8 @@ class _BaseStokEditingViewState extends BaseState<BaseStokEditingView> with Tick
   Future<void> postData() async {
     final StokListesiModel model = StokListesiModel.instance;
     final SaveStokModel saveStokModel = SaveStokModel.fromStokListesiModel(model);
-    saveStokModel.adi = model.stokAdi;
-    saveStokModel.kodu = model.stokKodu;
-    saveStokModel.alisFiyati1 = model.alisFiat1;
-    saveStokModel.alisDovizTipi = model.alisDovTip;
-    saveStokModel.satisDovizTipi = model.satDovTip;
-    saveStokModel.alisFiyati2 = model.alisFiat2;
-    // saveStokModel.resimBase64 = model.resimBase64;
-    saveStokModel.islemKodu = widget.model?.baseEditEnum == BaseEditEnum.ekle ? 1 : 2;
-    saveStokModel.yeniKayit = saveStokModel.islemKodu == 1 ? true : false;
-    // saveStokModel.subeKodu = model.subeKodu;
-    saveStokModel.alisFiyati3 = model.alisFiat3;
-    saveStokModel.alisFiyati4 = model.alisFiat4;
-    saveStokModel.olcuBirimi1 = model.olcuBirimi;
-    saveStokModel.seriGiristeAktif = model.seriGirislerdeAcik;
-    saveStokModel.seriCikistaAktif = model.seriCikislardaAcik;
-    saveStokModel.seriGiristeOtomatik = model.seriGiristeOtomatikMi;
-    saveStokModel.seriCikistaOtomatik = model.seriCikistaOtomatikMi;
-    saveStokModel.seriMiktarKadar = model.seriMiktarKadarSor;
-    saveStokModel.alisKdvOrani = model.alisKdv;
-    saveStokModel.satisKdvOrani = model.satisKdv;
-    saveStokModel.alisDovizFiyati = model.dovAlisFiat;
-    saveStokModel.satisDovizFiyati = model.dovSatisFiat;
-    saveStokModel.muhdetayKodu = model.muhdetayKodu;
-    saveStokModel.satisFiyati1 = model.satisFiat1;
-    saveStokModel.satisFiyati2 = model.satisFiat2;
-    saveStokModel.satisFiyati3 = model.satisFiat3;
-    saveStokModel.satisFiyati4 = model.satisFiat4;
-    saveStokModel.alisFiyati1 = model.alisFiat1;
-    saveStokModel.alisFiyati2 = model.alisFiat2;
-    saveStokModel.alisFiyati3 = model.alisFiat3;
-    saveStokModel.alisFiyati4 = model.alisFiat4;
-    saveStokModel.satisKdvOrani = model.satisKdv;
-    saveStokModel.alisKdvOrani = model.alisKdv;
-    saveStokModel.satisDovizFiyati = model.dovSatisFiat;
-    saveStokModel.alisDovizFiyati = model.dovAlisFiat;
-    saveStokModel.alisDovizTipi = model.alisDovTip;
-    saveStokModel.satisDovizTipi = model.satDovTip;
+    saveStokModel.requestVersion = widget.model?.baseEditEnum == BaseEditEnum.ekle ? 1 : 2;
+    saveStokModel.yeniKayit = widget.model?.baseEditEnum == BaseEditEnum.ekle || widget.model?.baseEditEnum == BaseEditEnum.kopyala;
     final result = await networkManager.dioPost<SaveStokModel>(
       path: ApiUrls.saveStok,
       bodyModel: SaveStokModel(),
@@ -162,11 +127,11 @@ class _BaseStokEditingViewState extends BaseState<BaseStokEditingView> with Tick
       addSirketBilgileri: true,
       data: saveStokModel.toJson(),
     );
-    SaveStokModel.setInstance(SaveStokModel());
     if (result.success == true) {
       dialogManager.showSuccessSnackBar("Başarılı");
-      Get.back(result: BaseEditModel());
+      Get.back(result: saveStokModel);
     }
+    SaveStokModel.setInstance(SaveStokModel());
   }
 
   Map<String, int> get validate {
