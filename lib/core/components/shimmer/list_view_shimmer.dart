09bbea59f64f25b15145564ed9f7d5469f88f49c
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-03 16:31:29 +0300

    Add shimmer dependency and implement ListViewShimmer

---
+++ lib/core/components/shimmer/list_view_shimmer.dart
@@ -0,0 +1,100 @@
+import "dart:math";
+
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+import "package:picker/core/base/state/base_state.dart";
+import "package:picker/core/constants/ui_helper/ui_helper.dart";
+import "package:shimmer/shimmer.dart";
+
+class ListViewShimmer extends StatefulWidget {
+  const ListViewShimmer({super.key});
+
+  @override
+  State<ListViewShimmer> createState() => _ListViewShimmerState();
+}
+
+class _ListViewShimmerState extends BaseState<ListViewShimmer> {
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Column(
+          children: List.generate(
+            10,
+            (index) => Shimmer.fromColors(
+              baseColor: Colors.grey.withOpacity(0.1),
+              highlightColor: Colors.grey.withOpacity(0.2),
+              
+              child: Card(
+                child: ListTile(
+                  leading: const CircleAvatar(),
+                  title: Container(
+                    width: 100,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      borderRadius: UIHelper.lowBorderRadius,
+                      color: Colors.white,
+                    ),
+                  ).paddingOnly(bottom: UIHelper.lowSize),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        //create a list of badges. size of badges is random
+                        children: List.generate(
+                          Random().nextInt(4),
+                          (index) => const Badge(
+                            label: Text("deneme"),
+                          ).paddingOnly(right: UIHelper.lowSize),
+                        ),
+                      ).paddingOnly(bottom: UIHelper.lowSize),
+                      Container(
+                        width: containerWidth,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          borderRadius: UIHelper.lowBorderRadius,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //   Shimmer.fromColors(
+          //     baseColor: UIHelper.primaryColor.withOpacity(0.5),
+          //     highlightColor: UIHelper.primaryColor.withOpacity(0.2),
+          //     // direction: ShimmerDirection.ttb,
+          //     child: Card(
+          //       child: ListTile(
+          //         leading: const CircleAvatar(),
+          //         title: Container(
+          //           width: 100,
+          //           height: 20,
+          //           color: Colors.white,
+          //         ).paddingOnly(bottom: UIHelper.lowSize),
+          //         subtitle: Container(
+          //           width: 50,
+          //           height: 15,
+          //           color: Colors.white,
+          //           child: Text(
+          //             "subtitle",
+          //             style: theme.textTheme.titleSmall,
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ],
+        ),
+      );
+
+  double get containerWidth {
+    while (true) {
+      final double widthOfContainer = Random().nextDouble() * (width * 0.5);
+      if (widthOfContainer > 100) {
+        return widthOfContainer;
+      }
+    }
+  }
+}
