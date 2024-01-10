741728e8e8cacb65595652826d2d567d349024ff
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-10 16:00:22 +0300

    Add rawValue getter to GrupKoduEnum

--- lib/core/constants/enum/grup_kodu_enums.dart
+++ lib/core/constants/enum/grup_kodu_enums.dart
@@ -1,1 +1,12 @@
 enum GrupKoduEnum { cari, stok }
+
+extension GrupKoduEnumExtension on GrupKoduEnum {
+  String get rawValue {
+    switch (this) {
+      case GrupKoduEnum.cari:
+        return "CARI";
+      case GrupKoduEnum.stok:
+        return "STOK";
+    }
+  }
+}
