4edfcf1e83d558215ff2cb747543011100cfae85
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-11 10:49:48 +0300

    Refactor UI padding and add CompanyModel class

---
+++ lib/view/auth/model/company_model.dart
@@ -0,0 +1,34 @@
+import "package:json_annotation/json_annotation.dart";
+
+import "../../../core/base/model/base_network_mixin.dart";
+
+part "company_model.g.dart";
+
+@JsonSerializable()
+class CompanyModel with NetworkManagerMixin {
+  @JsonKey(name: "SIRKET")
+  String? company;
+  @JsonKey(name: "YIL")
+  int? year;
+
+  CompanyModel();
+  @override
+  CompanyModel fromJson(dynamic json) => _$CompanyModelFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$CompanyModelToJson(this);
+
+  @override
+  String toString() => "company: $company,\n year: $year";
+
+  //operator == and hashCode
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CompanyModel && other.company == company && other.year == year;
+  }
+
+  @override
+  int get hashCode => company.hashCode ^ year.hashCode;
+}
