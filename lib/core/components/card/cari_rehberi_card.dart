2bfee4e84fd8ff4673799ece49d32a97d38b6e1a
brktlhylmz <buraktalha28.by@gmail.com>
2024-01-22 17:48:33 +0300

    Added new extensions and improved UI elements in cari_rehberi_card.dart

--- lib/core/components/card/cari_rehberi_card.dart
+++ lib/core/components/card/cari_rehberi_card.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 import "package:picker/core/constants/enum/badge_color_enum.dart";
+import "package:picker/core/constants/extensions/list_extensions.dart";
+import "package:picker/core/constants/extensions/widget_extensions.dart";
 
 import "../../../view/main_page/alt_sayfalar/cari/cari_listesi/model/cari_listesi_model.dart";
 import "../../base/state/base_state.dart";
@@ -38,13 +40,21 @@ class _CariRehberiCardState extends BaseState<CariRehberiCard> {
             alignment: WrapAlignment.spaceBetween,
             children: [
               Text("${model.cariKodu}", style: TextStyle(color: theme.textTheme.bodySmall?.color?.withOpacity(0.5))),
-              if (model.cariIl != null) Text("${model.cariIl}/${model.cariIlce}", style: TextStyle(color: theme.textTheme.bodySmall?.color?.withOpacity(0.5))) else const SizedBox(),
+              if (model.cariIl != null) Text("${model.cariIl ?? ""}/${model.cariIlce ?? ""}", style: TextStyle(color: theme.textTheme.bodySmall?.color?.withOpacity(0.5))) else const SizedBox(),
               Row(
                 children: [
-                  if (model.efaturaMi == true) const ColorfulBadge(label: Text("E-Fatura"), badgeColorEnum: BadgeColorEnum.eFatura) else const SizedBox(),
-                  if (model.boylam != null && model.enlem != null) const ColorfulBadge(label: Text("Konum"), badgeColorEnum: BadgeColorEnum.konum) else const SizedBox(),
-                  if (model.dovizli == true) ColorfulBadge(label: Text("Dövizli ${model.dovizAdi ?? ""}"), badgeColorEnum: BadgeColorEnum.dovizli) else const SizedBox(),
-                ].map((e) => e.paddingOnly(top: UIHelper.lowSize, right: UIHelper.lowSize)).toList(),
+                  const ColorfulBadge(label: Text("E-Fatura"), badgeColorEnum: BadgeColorEnum.fatura).yetkiVarMi(model.efaturaMi == true),
+                  ColorfulBadge(label: Text("Dövizli ${model.dovizAdi ?? ""}"), badgeColorEnum: BadgeColorEnum.dovizli).yetkiVarMi(model.dovizli == true),
+                  const ColorfulBadge(label: Text("Konum"), badgeColorEnum: BadgeColorEnum.konum).yetkiVarMi(model.boylam != null),
+                  const ColorfulBadge(label: Text("Kilitli"), badgeColorEnum: BadgeColorEnum.kilitli).yetkiVarMi(model.kilit == "E"),
+                  // object.boylam != null && object.enlem != null ? const Badge(label: Text(("Konum"))) : const SizedBox(),
+                  // object.dovizAdi != null ? Badge(label: Text(("Dövizli ${object.dovizAdi}"))) : const SizedBox(),
+                ]
+                    .map(
+                      (e) => e is! SizedBox? ? e.paddingOnly(top: UIHelper.lowSize, right: UIHelper.lowSize) : null,
+                    )
+                    .toList()
+                    .nullCheckWithGeneric,
               ),
             ],
           ),
@@ -63,3 +73,94 @@ class _CariRehberiCardState extends BaseState<CariRehberiCard> {
         ),
       );
 }
+// if (index < (viewModel.cariListesi?.length ?? 0)) {
+//                       final CariListesiModel object = viewModel.cariListesi?[index];
+//                       return Card(
+//                         child: Listener(
+//                           onPointerDown: (event) {
+//                             if (event.kind == PointerDeviceKind.mouse && event.buttons == 2) {
+//                               showCariGrid(object);
+//                             }
+//                           },
+//                           child: ListTile(
+//                             onLongPress: () {
+//                               showCariGrid(object);
+//                             },
+//                             onTap: () {
+//                               if (widget.isGetData) {
+//                                 Get.back(result: object);
+//                               } else {
+//                                 cariBottomSheet(context, object);
+//                               }
+//                             },
+//                             isThreeLine: true,
+//                             // contentPadding: UIHelper.midPadding,
+//                             leading: CircleAvatar(
+//                               backgroundColor: UIHelper.getColorWithValue(object.bakiye ?? 0.0),
+//                               foregroundColor: Colors.white,
+//                               child: Text(object.cariAdi?.substring(0, 1) ?? "", style: const TextStyle(color: Colors.white)),
+//                             ),
+//                             title: Text(object.cariAdi ?? ""),
+//                             subtitle: Wrap(
+//                               direction: Axis.vertical,
+//                               alignment: WrapAlignment.spaceBetween,
+//                               children: [
+//                                 Text("${object.cariKodu}", style: TextStyle(color: theme.textTheme.bodySmall?.color?.withOpacity(0.5))),
+//                                 if (object.cariIl != null)
+//                                   Text("${object.cariIl ?? ""}/${object.cariIlce ?? ""}", style: TextStyle(color: theme.textTheme.bodySmall?.color?.withOpacity(0.5)))
+//                                 else
+//                                   const SizedBox(),
+//                                 Row(
+//                                   children: [
+//                                     const ColorfulBadge(label: Text("E-Fatura"), badgeColorEnum: BadgeColorEnum.fatura).yetkiVarMi(object.efaturaMi == true),
+//                                     ColorfulBadge(label: Text("Dövizli ${object.dovizAdi ?? ""}"), badgeColorEnum: BadgeColorEnum.dovizli).yetkiVarMi(object.dovizli == true),
+//                                     const ColorfulBadge(label: Text("Konum"), badgeColorEnum: BadgeColorEnum.konum).yetkiVarMi(object.boylam != null),
+//                                     const ColorfulBadge(label: Text("Kilitli"), badgeColorEnum: BadgeColorEnum.kilitli).yetkiVarMi(object.kilit == "E"),
+//                                     // object.boylam != null && object.enlem != null ? const Badge(label: Text(("Konum"))) : const SizedBox(),
+//                                     // object.dovizAdi != null ? Badge(label: Text(("Dövizli ${object.dovizAdi}"))) : const SizedBox(),
+//                                   ]
+//                                       .map(
+//                                         (e) => e is! SizedBox? ? e.paddingOnly(top: UIHelper.lowSize, right: UIHelper.lowSize) : null,
+//                                       )
+//                                       .toList()
+//                                       .nullCheckWithGeneric,
+//                                 ),
+//                               ],
+//                             ),
+//                             trailing: Wrap(
+//                               children: [
+//                                 Text.rich(
+//                                   TextSpan(
+//                                     children: [
+//                                       TextSpan(
+//                                         text: object.bakiye == null ? "0,00 $mainCurrency\n" : "${object.bakiye.commaSeparatedWithDecimalDigits(OndalikEnum.tutar)} $mainCurrency\n",
+//                                         style: TextStyle(color: UIHelper.getColorWithValue(object.bakiye ?? 0.0)),
+//                                       ),
+//                                       if (object.bakiye != null)
+//                                         TextSpan(text: "${((object.bakiye ?? 0) > 0) ? "Tahsil Edilecek" : "Ödenecek"}\n", style: const TextStyle(fontStyle: FontStyle.italic))
+//                                       else
+//                                         null,
+//                                       if (object.dovizli == true && object.dovBakiye != null)
+//                                         TextSpan(
+//                                           text: "${object.dovBakiye?.commaSeparatedWithDecimalDigits(OndalikEnum.tutar) ?? ""} ${object.dovizAdi ?? ""}",
+//                                           style: TextStyle(color: UIHelper.getColorWithValue(object.dovBakiye ?? 0.0)),
+//                                         )
+//                                       else
+//                                         null,
+//                                     ].nullCheckWithGeneric,
+//                                   ),
+//                                   textAlign: TextAlign.right,
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                         ),
+//                       );
+//                     } else {
+//                       return Observer(
+//                         builder: (_) => Visibility(
+//                           visible: viewModel.dahaVarMi,
+//                           child: const Center(child: CircularProgressIndicator.adaptive()),
+//                         ),
+//                       );
+//                     }
