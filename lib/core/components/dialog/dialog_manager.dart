aa91343e24a854a75a2a0bb5a0af3ae49d904355
brktlhylmz <buraktalha28.by@gmail.com>
2024-02-06 17:07:31 +0300

    add showTransferGridViewDialog

--- lib/core/components/dialog/dialog_manager.dart
+++ lib/core/components/dialog/dialog_manager.dart
@@ -241,6 +241,12 @@ class DialogManager {
         btnOkText: "İptal",
         dialogType: DialogType.noHeader,
       ).show();
+  Future<dynamic> showTransferGridViewDialog({BaseSiparisEditModel? model, IslemTipiEnum? tip, EditTipiEnum? siparisTipi, Function(bool)? onSelected}) async => await _baseDialog(
+        body: CustomAnimatedGridView<BaseSiparisEditModel>(model: model, islemTipi: tip ?? IslemTipiEnum.depoTransferi, siparisTipi: siparisTipi, title: model?.belgeNo, onSelected: onSelected),
+        onOk: () {},
+        btnOkText: "İptal",
+        dialogType: DialogType.noHeader,
+      ).show();
   Future<dynamic> showKasaGridViewDialog(KasaListesiModel? model, {IslemTipiEnum? tip, Function(bool)? onSelected}) async => await _baseDialog(
         body: CustomAnimatedGridView<KasaListesiModel>(model: model, islemTipi: tip ?? IslemTipiEnum.kasa, title: "Kasa İşlemleri", onSelected: onSelected),
         onOk: () {},
