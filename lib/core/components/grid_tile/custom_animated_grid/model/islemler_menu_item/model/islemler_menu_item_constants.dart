e64fcb8b51a2c92247f499156693d85b900aa559
brktlhylmz <buraktalha28.by@gmail.com>
2024-03-14 12:50:25 +0300

    bug solved.

--- lib/core/components/grid_tile/custom_animated_grid/model/islemler_menu_item/model/islemler_menu_item_constants.dart
+++ lib/core/components/grid_tile/custom_animated_grid/model/islemler_menu_item/model/islemler_menu_item_constants.dart
@@ -100,7 +100,7 @@ class IslemlerMenuItemConstants<T> {
         islemlerList.addIfConditionTrue(_yetkiController.cariKartiYeniKayit, kopyala);
         islemlerList.add(cariHareketleri);
         islemlerList.add(stokHareketleri);
-        islemlerList.addIfConditionTrue(_yetkiController., cariAktivite);
+        islemlerList.add(cariAktivite);
         islemlerList.add(cariKoduDegistir(newModel.cariKodu));
         islemlerList.addAll(raporlar ?? []);
       }
@@ -228,7 +228,7 @@ class IslemlerMenuItemConstants<T> {
       GridItemModel.islemler(iconData: Icons.sync_alt_outlined, title: "Stok Hareketleri", onTap: () async => Get.toNamed("mainPage/stokHareketleri", arguments: model));
   GridItemModel? get cariAktivite => GridItemModel.islemler(
         iconData: Icons.sync_alt_outlined,
-        //TODO ISDEBUG KALDIR
+        //TODO ISDEBUG KALDIR 
         isEnabled: _yetkiController.cariAktivite && AccountModel.instance.isDebug,
         title: "Cari Aktivite",
         onTap: () async => Get.toNamed("mainPage/cariAktivite", arguments: model),
