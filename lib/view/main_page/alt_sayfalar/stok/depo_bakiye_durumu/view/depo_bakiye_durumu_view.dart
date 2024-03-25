db682446b20564ca5de7fc7b4040b3fb780d0411
brktlhylmz <buraktalha28.by@gmail.com>
2024-03-25 15:47:14 +0300

    depo bakiye durumu is completed.

--- lib/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/view/depo_bakiye_durumu_view.dart
+++ lib/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/view/depo_bakiye_durumu_view.dart
@@ -1,7 +1,16 @@
 import "package:flutter/material.dart";
 import "package:flutter_mobx/flutter_mobx.dart";
+import "package:get/get.dart";
 import "package:picker/core/base/state/base_state.dart";
+import "package:picker/core/components/bottom_bar/bottom_bar.dart";
+import "package:picker/core/components/button/elevated_buttons/footer_button.dart";
+import "package:picker/core/components/dialog/bottom_sheet/model/bottom_sheet_model.dart";
+import "package:picker/core/components/shimmer/list_view_shimmer.dart";
 import "package:picker/core/components/wrap/appbar_title.dart";
+import "package:picker/core/constants/extensions/number_extensions.dart";
+import "package:picker/core/constants/ondalik_utils.dart";
+import "package:picker/core/constants/ui_helper/ui_helper.dart";
+import "package:picker/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/model/depo_bakiye_durumu_model.dart";
 import "package:picker/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/view_model/depo_bakiye_durumu_view_model.dart";
 import "package:picker/view/main_page/alt_sayfalar/stok/stok_liste/model/stok_listesi_model.dart";
 
@@ -29,17 +38,101 @@ class _DepoBakiyeDurumuViewState extends BaseState<DepoBakiyeDurumuView> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: AppBarTitle(
-            title: "Depo Bakiye Durumu",
-            subtitle: widget.model?.stokAdi,
-          ),
+        appBar: appBar(),
+        bottomNavigationBar: bottomAppBar(),
+        body: body(),
+      );
+
+  AppBar appBar() => AppBar(
+        title: AppBarTitle(
+          title: "Depo Bakiye Durumu",
+          subtitle: widget.model?.stokAdi,
         ),
-        body: Observer(
-          builder: (_) => ListView.builder(
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await bottomSheetDialogManager.showBottomSheetDialog(
+                context,
+                title: loc.generalStrings.options,
+                children: [
+                  BottomSheetModel(
+                    title: "Sıfır Bakiye Hariç",
+                    iconWidget: viewModel.dicParams.sifirHaric == "E" ? Icons.check_box_outlined : Icons.check_box_outline_blank_outlined,
+                    onTap: () async {
+                      Get.back();
+                      viewModel.setSifirHaric(viewModel.dicParams.sifirHaric != "E");
+                      await viewModel.getData();
+                    },
+                  ),
+                ],
+              );
+            },
+            icon: const Icon(Icons.more_vert_outlined),
+          ),
+        ],
+      );
+
+  BottomBarWidget bottomAppBar() => BottomBarWidget(
+        isScrolledDown: true,
+        children: [
+          FooterButton(
+            children: [
+              const Text("Giriş"),
+              Observer(
+                builder: (_) => Text(viewModel.toplamGiris.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)),
+              ),
+            ],
+          ),
+          FooterButton(
+            children: [
+              const Text("Çıkış"),
+              Observer(
+                builder: (_) => Text(viewModel.toplamCikis.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)),
+              ),
+            ],
+          ),
+          FooterButton(
+            children: [
+              const Text("Bakiye"),
+              Observer(
+                builder: (_) => Text(viewModel.toplamBakiye.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)),
+              ),
+            ],
+          ),
+        ],
+      );
+
+  Observer body() => Observer(
+        builder: (_) {
+          if (viewModel.depoBakiyeDurumuList == null) {
+            return const ListViewShimmer();
+          } else if (viewModel.depoBakiyeDurumuList?.isEmpty ?? true) {
+            return const Center(
+              child: Text("Depo Bakiye Durumu Bulunamadı"),
+            );
+          }
+          return ListView.builder(
             itemCount: viewModel.subeAdiList?.length ?? 0,
-            itemBuilder: (context, index) => Card(child: ListView.builder(itemCount: 5, itemBuilder: (context, index2) => Text(index2.toString()))),
-          ),
-        ),
+            itemBuilder: (context, index) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(viewModel.subeAdiList?[index] ?? "", style: const TextStyle(fontWeight: FontWeight.bold)).paddingSymmetric(horizontal: UIHelper.lowSize),
+                ...List.generate(
+                  viewModel.depoBakiyeDurumuList?.where((element) => element.subeAdi == viewModel.subeAdiList?[index]).length ?? 0,
+                  (index) {
+                    final DepoBakiyeDurumuModel? item = viewModel.depoBakiyeDurumuList?[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text("${item?.depoAdi}"),
+                        subtitle: Text("Depo Kodu: ${item?.depoKodu}"),
+                        trailing: Text("${item?.bakiye.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)}"),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ).paddingAll(UIHelper.lowSize),
+          );
+        },
       );
 }
--- lib/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/view_model/depo_bakiye_durumu_view_model.dart
+++ lib/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/view_model/depo_bakiye_durumu_view_model.dart
@@ -1,6 +1,7 @@
 import "package:mobx/mobx.dart";
 import "package:picker/core/base/view/pdf_viewer/model/pdf_viewer_model.dart";
 import "package:picker/core/base/view_model/mobx_network_mixin.dart";
+import "package:picker/core/constants/extensions/iterable_extensions.dart";
 import "package:picker/core/init/network/login/api_urls.dart";
 import "package:picker/view/main_page/alt_sayfalar/stok/depo_bakiye_durumu/model/depo_bakiye_durumu_model.dart";
 
@@ -18,6 +19,15 @@ abstract class _DepoBakiyeDurumuViewModelBase with Store, MobxNetworkMixin {
   @computed
   List<String>? get subeAdiList => depoBakiyeDurumuList?.map((e) => e.subeAdi ?? "").toSet().toList();
 
+  @computed
+  double get toplamGiris => depoBakiyeDurumuList?.where((element) => element.depoKodu == 0).map((element) => element.giris).sum ?? 0;
+
+  @computed
+  double get toplamCikis => depoBakiyeDurumuList?.where((element) => element.depoKodu == 0).map((element) => element.cikis).sum ?? 0;
+
+  @computed
+  double get toplamBakiye => depoBakiyeDurumuList?.where((element) => element.depoKodu == 0).map((element) => element.bakiye).sum ?? 0;
+
   @action
   void setSifirHaric(bool? sifirHaric) => dicParams = dicParams.copyWith(sifirHaric: sifirHaric == true ? "E" : "H");
 
@@ -29,8 +39,12 @@ abstract class _DepoBakiyeDurumuViewModelBase with Store, MobxNetworkMixin {
 
   @action
   Future<void> getData() async {
-    final result =
-        await networkManager.dioGet(path: ApiUrls.getStokDepoDurum, bodyModel: DepoBakiyeDurumuModel(), queryParameters: {"StokKodu": dicParams.stokKodu, "SifirHaric": dicParams.sifirHaric});
+    final result = await networkManager.dioGet(
+      path: ApiUrls.getStokDepoDurum,
+      bodyModel: const DepoBakiyeDurumuModel(),
+      queryParameters: {"StokKodu": dicParams.stokKodu, "SifirHaric": dicParams.sifirHaric},
+      showLoading: true,
+    );
     if (result.success == true) {
       final List<DepoBakiyeDurumuModel> list = (result.data as List).map((e) => e as DepoBakiyeDurumuModel).toList();
       setDepoBakiyeDurumuList(list);
