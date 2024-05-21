51b7c01f0fdcc85f579fa4e15b8d6ff4837a5dff
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-05-21 18:00:23 +0300

    update for stok Oluştur stok kodu bug

--- lib/view/main_page/alt_sayfalar/mal_kabul_sevkiyat/faturalar/base_fatura_edit/alt_sayfalar/base_fatura_kalemler/view/base_fatura_kalemler_view.dart
+++ lib/view/main_page/alt_sayfalar/mal_kabul_sevkiyat/faturalar/base_fatura_edit/alt_sayfalar/base_fatura_kalemler/view/base_fatura_kalemler_view.dart
@@ -319,7 +319,7 @@ class _BaseFaturaKalemlerViewState extends BaseState<BaseFaturaKalemlerView> {
             Get.back();
             final result = await Get.toNamed(
               "/mainPage/stokEdit",
-              arguments: BaseEditModel<StokListesiModel>(model: StokListesiModel.fromKalemModel((model?..stokKodu = null)!), baseEditEnum: BaseEditEnum.ekle),
+              arguments: BaseEditModel<StokListesiModel>(model: StokListesiModel.fromKalemModel((model?.copyWith(stokKodu: null))!), baseEditEnum: BaseEditEnum.ekle),
             );
             if (result is SaveStokModel) {
               BaseSiparisEditModel.instance.kalemList?[index] = viewModel.kalemList![index].copyWith(
