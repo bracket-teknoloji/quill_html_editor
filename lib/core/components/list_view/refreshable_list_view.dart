690c4ecd87eaf91060edc86f8d7f41e038e353d2
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-05-29 18:01:59 +0300

    refreshable listview added.

---
+++ lib/core/components/list_view/refreshable_list_view.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+import "package:picker/core/base/model/base_network_mixin.dart";
+import "package:picker/core/components/shimmer/list_view_shimmer.dart";
+import "package:picker/core/constants/ui_helper/ui_helper.dart";
+
+class RefreshableListView<T extends NetworkManagerMixin> extends StatelessWidget {
+  final Future<void> Function() onRefresh;
+  final List<T>? items;
+  final Widget Function(T) itemBuilder;
+  const RefreshableListView({super.key,required this.onRefresh, this.items, required this.itemBuilder});
+
+  @override
+  Widget build(BuildContext context) => RefreshIndicator.adaptive(
+        onRefresh: onRefresh,
+        child: body(),
+      ).paddingAll(UIHelper.lowSize);
+
+  Widget body() {
+    if (items == null) return const ListViewShimmer();
+    if (items!.isEmpty) return const Center(child: Text("Liste bulunamadı."));
+    return ListView.builder(  
+      itemCount: items!.length,
+      itemBuilder: (context, index) => itemBuilder(items![index]),
+    );
+  }
+}
