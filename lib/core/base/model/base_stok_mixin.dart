3a8374f23ab82df422ebd2515bc30cebd2f167df
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-05-31 14:56:42 +0300

    added miktar and added comments of mixin

--- lib/core/base/model/base_stok_mixin.dart
+++ lib/core/base/model/base_stok_mixin.dart
@@ -1,10 +1,15 @@
+
+
+/// Bu mixin, stok bilgilerini tutan model sınıflarında kullanılır.
+/// stokKodu ve StokAdi ortaklaştırma amacıyla yapılmıştır.
 mixin BaseStokMixin {
   String? get stokKodu;
   String? get stokAdi;
-  String? get olcuBirimi;
+  String? get olcuBirimi => "";
 
   double? get netMiktar;
+  double? get miktar;
   double? get bakiye;
 
-  double? get getMiktar => netMiktar ?? bakiye;
+  double? get getMiktar => netMiktar ?? bakiye ?? miktar;
 }
