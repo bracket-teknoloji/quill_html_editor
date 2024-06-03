bb81b318d8497278bc12c1b574aa36f0bfccbada
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-06-03 18:01:48 +0300

    getOlcuBirimi added

--- lib/core/base/model/base_stok_mixin.dart
+++ lib/core/base/model/base_stok_mixin.dart
@@ -1,4 +1,4 @@
-
+import "package:picker/core/constants/extensions/string_extensions.dart";
 
 /// Bu mixin, stok bilgilerini tutan model sınıflarında kullanılır.
 /// stokKodu ve StokAdi ortaklaştırma amacıyla yapılmıştır.
@@ -6,6 +6,8 @@ mixin BaseStokMixin {
   String? get stokKodu;
   String? get stokAdi;
   String? get olcuBirimi => "";
+  String? get belgeOlcuBirimAdi => "";
+  String? get getOlcuBirimi => olcuBirimi?.withoutBlanks ?? belgeOlcuBirimAdi.withoutBlanks;
 
   double? get netMiktar;
   double? get miktar;
