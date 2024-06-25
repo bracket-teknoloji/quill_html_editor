91a0fb28dcdded4d95de530be1d4990842b5f298
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-06-25 16:37:40 +0300

    RefreshableListView.pageable added and documentation updates

--- lib/core/components/list_view/refreshable_list_view.dart
+++ lib/core/components/list_view/refreshable_list_view.dart
@@ -6,23 +6,57 @@ import "package:picker/core/constants/ui_helper/ui_helper.dart";
 
 /// Bu widget ile Sayfalama işlemleri yapılır.
 ///
-/// Sayfayı yukarı kaydırarak yenileme işlemi yapılmaktadır.
-/// Yenileme işlemi yapıldığında sayfa yenilenir.
-/// Bu widget'ın State'ini yönetmek için `Observer` widget'ı kullanınız.
+///* Sayfayı yukarı kaydırarak yenileme işlemi yapılmaktadır.
+///* Yenileme işlemi yapıldığında sayfa yenilenir.
+///* Bu widget'ın State'ini yönetmek için `Observer` widget'ı kullanınız.
 class RefreshableListView<T extends NetworkManagerMixin> extends StatelessWidget {
-  ///* Refresh işlemleri için gerekli fonksiyon
+  /// Refresh işlemleri için gerekli fonksiyon
   final Future<void> Function() onRefresh;
 
-  ///* Liste elemanları
+  /// Liste elemanları
   final List<T>? items;
 
-  ///* Liste elemanlarının görünümü için builder
+  /// Liste elemanlarının görünümü için builder
   final Widget Function(T item) itemBuilder;
 
-  ///* Tek istekle bütün verilerin geldiği durumda bunu kullanınız.
-  const RefreshableListView({super.key, required this.onRefresh, required this.items, required this.itemBuilder});
+  /// Bu widget'ın sayfalama yapısına ait olup olmadığını kontrol etmek için kullanılır.
+  /// Sadece constructor üzerinden çağırılıp değiştirilebilir.
+  final bool _isPageable;
 
-  ///* Bu widget adaptive olarak çalışmaktadır. Padding'i önden tanımlanmıştır.
+  /// Liste sayfalama yapısına sahipse kullanılır.
+  /// Bunun sayesinde ekranda [CircularProgressIndicator] görünüp görünmemesini sağlarız.
+  /// Eğer [true] ise [CircularProgressIndicator] görünür.
+  final bool dahaVarMi;
+
+  /// Bu widget sayfalama yapısına ait ise istenir.
+  /// Sayfa hareketlerini takip etmek içn kullanılır.
+  final ScrollController? scrollController;
+
+  /// Tek istekle bütün verilerin geldiği durumda bunu kullanınız.
+  const RefreshableListView({
+    super.key,
+    required this.onRefresh,
+    required this.items,
+    required this.itemBuilder,
+  })  : _isPageable = false,
+        dahaVarMi = false,
+        scrollController = null;
+
+  /// Eğer ihtiyaç olunan veriler tek bir istek ile gelmiyorsa bunu kullanınız.
+  /// Sayfalama özelliklerini kullanacağınız durumlarda işinize yarayacaktır.
+  ///* [scrollController] ile sayfa hareketlerini takip edebilirsiniz.
+  ///* [dahaVarMi] ile ekranda [CircularProgressIndicator] görünmemesini sağlayabilirsiniz.
+  const RefreshableListView.pageable({
+    super.key,
+    required this.scrollController,
+    required this.onRefresh,
+    required this.dahaVarMi,
+    required this.items,
+    required this.itemBuilder,
+  }) : _isPageable = true;
+
+  ///Bu widget adaptive olarak çalışmaktadır.
+  ///Padding'i önden tanımlanmıştır.
   @override
   Widget build(BuildContext context) => RefreshIndicator.adaptive(
         onRefresh: onRefresh,
@@ -32,10 +66,28 @@ class RefreshableListView<T extends NetworkManagerMixin> extends StatelessWidget
   Widget body() {
     if (items == null) return const ListViewShimmer();
     if (items!.isEmpty) return const Center(child: Text("Liste bulunamadı."));
+    if (!_isPageable) {
+      return ListView.builder(
+        // items nullcheck yapıldığı için ünlem koyabiliriz.
+        itemCount: items!.length,
+        itemBuilder: (context, index) => itemBuilder(items![index]),
+      );
+    }
+
     return ListView.builder(
-      // items nullcheck yapıldığı için ünlem koyabiliriz.
-      itemCount: items!.length,
-      itemBuilder: (context, index) => itemBuilder(items![index]),
+      controller: scrollController,
+      // DahaVarMi [true] ise [CircularProgressIndicator] görünür.
+      // Gösterebilmek için item sayısını 1 artırırız.
+      itemCount: dahaVarMi ? items!.length + 1 : items!.length,
+      itemBuilder: (context, index) {
+        if (index == items!.length) {
+          return Visibility(
+            visible: dahaVarMi,
+            child: const Center(child: CircularProgressIndicator.adaptive()),
+          );
+        }
+        return itemBuilder(items![index]);
+      },
     );
   }
 }
