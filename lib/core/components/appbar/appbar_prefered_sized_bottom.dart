bc0b6a91f9fa8327dbc11f60525890b8acb26f3b
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-08-02 17:41:23 +0300

    filter size updated.

--- lib/core/components/appbar/appbar_prefered_sized_bottom.dart
+++ lib/core/components/appbar/appbar_prefered_sized_bottom.dart
@@ -8,32 +8,44 @@ import "../button/elevated_buttons/bottom_appbar_button.dart";
 
 class AppBarPreferedSizedBottom extends StatefulWidget implements PreferredSizeWidget {
   final List<AppBarButton?> children;
-  const AppBarPreferedSizedBottom({super.key, required this.children});
+  final Widget? filter;
+
+  const AppBarPreferedSizedBottom({super.key, required this.children}) : filter = null;
+  const AppBarPreferedSizedBottom.forStokFilter({super.key, required this.children, required this.filter});
 
   @override
   State<AppBarPreferedSizedBottom> createState() => _AppBarPreferedSizedBottomState();
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(toolbarHeight());
+
+  double toolbarHeight() => kToolbarHeight * (filter is! SizedBox ? 3 : 1);
 }
 
 class _AppBarPreferedSizedBottomState extends BaseState<AppBarPreferedSizedBottom> {
   @override
   Widget build(BuildContext context) => SizedBox(
-        height: kToolbarHeight,
-        child: ListView(
-          shrinkWrap: true,
-          itemExtent: width * (1 / widget.children.nullCheck.length < 0.2 ? 0.2 : 1 / widget.children.nullCheck.length),
-          scrollDirection: Axis.horizontal,
-          children: widget.children.nullCheck
-              .map(
-                (e) => e?.paddingSymmetric(
-                  horizontal: UIHelper.lowSize,
-                  vertical: UIHelper.lowSize,
-                ),
-              )
-              .toList()
-              .nullCheckWithGeneric,
+        height: widget.toolbarHeight(),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                itemExtent: width * (1 / widget.children.nullCheck.length < 0.2 ? 0.2 : 1 / widget.children.nullCheck.length),
+                scrollDirection: Axis.horizontal,
+                children: widget.children.nullCheck
+                    .map(
+                      (e) => e?.paddingSymmetric(
+                        horizontal: UIHelper.lowSize,
+                        vertical: UIHelper.lowSize,
+                      ),
+                    )
+                    .toList()
+                    .nullCheckWithGeneric,
+              ),
+            ),
+            widget.filter,
+          ].nullCheckWithGeneric,
         ),
       );
 }
