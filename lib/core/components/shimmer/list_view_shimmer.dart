b7af27c5a0b0d2024f5dedae2667f36e91211240
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2024-08-09 17:44:07 +0300

    ListViewShimmer code refactoring

--- lib/core/components/shimmer/list_view_shimmer.dart
+++ lib/core/components/shimmer/list_view_shimmer.dart
@@ -15,76 +15,49 @@ class ListViewShimmer extends StatefulWidget {
 
 class _ListViewShimmerState extends State<ListViewShimmer> {
   @override
-  Widget build(BuildContext context) => SingleChildScrollView(
+  Widget build(BuildContext context) => ListView(
         physics: const NeverScrollableScrollPhysics(),
-        child: Column(
-          children: List.generate(
-            10,
-            (index) => Shimmer.fromColors(
-              baseColor: Colors.grey.withOpacity(0.1),
-              highlightColor: Colors.grey.withOpacity(0.2),
-              child: Card(
-                child: ListTile(
-                  leading: const CircleAvatar(),
-                  title: Container(
-                    width: 100,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      borderRadius: UIHelper.lowBorderRadius,
-                      color: Colors.white,
-                    ),
-                  ).paddingOnly(bottom: UIHelper.lowSize),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        //create a list of badges. size of badges is random
-                        children: List.generate(
-                          Random().nextInt(4),
-                          (index) => const Badge(
-                            label: Text("deneme"),
-                          ).paddingOnly(right: UIHelper.lowSize),
-                        ),
-                      ).paddingOnly(bottom: UIHelper.lowSize),
-                      Container(
-                        width: containerWidth,
-                        height: 15,
-                        decoration: BoxDecoration(
-                          borderRadius: UIHelper.lowBorderRadius,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+        children: List.generate(
+          10,
+          (index) => Shimmer.fromColors(
+            baseColor: Colors.grey.withOpacity(0.1),
+            highlightColor: Colors.grey.withOpacity(0.2),
+            child: Card(
+              child: ListTile(
+                leading: const CircleAvatar(),
+                title: Container(
+                  width: 100,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    borderRadius: UIHelper.lowBorderRadius,
+                    color: Colors.white,
                   ),
+                ).paddingOnly(bottom: UIHelper.lowSize),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      //create a list of badges. size of badges is random
+                      children: List.generate(
+                        Random().nextInt(4),
+                        (index) => const Badge(
+                          label: Text("deneme"),
+                        ).paddingOnly(right: UIHelper.lowSize),
+                      ),
+                    ).paddingOnly(bottom: UIHelper.lowSize),
+                    Container(
+                      width: containerWidth,
+                      height: 15,
+                      decoration: BoxDecoration(
+                        borderRadius: UIHelper.lowBorderRadius,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          //   Shimmer.fromColors(
-          //     baseColor: UIHelper.primaryColor.withOpacity(0.5),
-          //     highlightColor: UIHelper.primaryColor.withOpacity(0.2),
-          //     // direction: ShimmerDirection.ttb,
-          //     child: Card(
-          //       child: ListTile(
-          //         leading: const CircleAvatar(),
-          //         title: Container(
-          //           width: 100,
-          //           height: 20,
-          //           color: Colors.white,
-          //         ).paddingOnly(bottom: UIHelper.lowSize),
-          //         subtitle: Container(
-          //           width: 50,
-          //           height: 15,
-          //           color: Colors.white,
-          //           child: Text(
-          //             "subtitle",
-          //             style: theme.textTheme.titleSmall,
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // ],
         ),
       );
 
