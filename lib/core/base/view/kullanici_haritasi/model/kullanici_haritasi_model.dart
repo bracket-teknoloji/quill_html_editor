d66a58edbc913791dbb593e38db0fb8a00c79814
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-03 18:05:35 +0300

    feat: add KullaniciHaritasiModel with JSON serialization and network management

---
+++ lib/core/base/view/kullanici_haritasi/model/kullanici_haritasi_model.dart
@@ -0,0 +1,27 @@
+import "package:freezed_annotation/freezed_annotation.dart";
+import "package:picker/core/base/model/base_network_mixin.dart";
+
+part "kullanici_haritasi_model.freezed.dart";
+part "kullanici_haritasi_model.g.dart";
+
+@unfreezed
+class KullaniciHaritasiModel with _$KullaniciHaritasiModel, NetworkManagerMixin {
+  factory KullaniciHaritasiModel({
+    String? kullanici,
+    String? kullaniciAdsoyad,
+    DateTime? tarih,
+    double? enlem,
+    double? boylam,
+    String? cihazMarka,
+    String? cihazModel,
+    String? cihazId,
+    int? hiz,
+  }) = _KullaniciHaritasiModel;
+  KullaniciHaritasiModel._();
+
+  @override
+  factory KullaniciHaritasiModel.fromJson(Map<String, dynamic> json) => _$KullaniciHaritasiModelFromJson(json);
+
+  @override
+  KullaniciHaritasiModel? fromJson(Map<String, dynamic> json) => _$KullaniciHaritasiModelFromJson(json);
+}
---
+++ lib/core/base/view/kullanici_haritasi/model/kullanici_haritasi_model.freezed.dart
@@ -0,0 +1,329 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'kullanici_haritasi_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+KullaniciHaritasiModel _$KullaniciHaritasiModelFromJson(
+    Map<String, dynamic> json) {
+  return _KullaniciHaritasiModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$KullaniciHaritasiModel {
+  String? get kullanici => throw _privateConstructorUsedError;
+  set kullanici(String? value) => throw _privateConstructorUsedError;
+  String? get kullaniciAdsoyad => throw _privateConstructorUsedError;
+  set kullaniciAdsoyad(String? value) => throw _privateConstructorUsedError;
+  DateTime? get tarih => throw _privateConstructorUsedError;
+  set tarih(DateTime? value) => throw _privateConstructorUsedError;
+  double? get enlem => throw _privateConstructorUsedError;
+  set enlem(double? value) => throw _privateConstructorUsedError;
+  double? get boylam => throw _privateConstructorUsedError;
+  set boylam(double? value) => throw _privateConstructorUsedError;
+  String? get cihazMarka => throw _privateConstructorUsedError;
+  set cihazMarka(String? value) => throw _privateConstructorUsedError;
+  String? get cihazModel => throw _privateConstructorUsedError;
+  set cihazModel(String? value) => throw _privateConstructorUsedError;
+  String? get cihazId => throw _privateConstructorUsedError;
+  set cihazId(String? value) => throw _privateConstructorUsedError;
+  int? get hiz => throw _privateConstructorUsedError;
+  set hiz(int? value) => throw _privateConstructorUsedError;
+
+  /// Serializes this KullaniciHaritasiModel to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of KullaniciHaritasiModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $KullaniciHaritasiModelCopyWith<KullaniciHaritasiModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $KullaniciHaritasiModelCopyWith<$Res> {
+  factory $KullaniciHaritasiModelCopyWith(KullaniciHaritasiModel value,
+          $Res Function(KullaniciHaritasiModel) then) =
+      _$KullaniciHaritasiModelCopyWithImpl<$Res, KullaniciHaritasiModel>;
+  @useResult
+  $Res call(
+      {String? kullanici,
+      String? kullaniciAdsoyad,
+      DateTime? tarih,
+      double? enlem,
+      double? boylam,
+      String? cihazMarka,
+      String? cihazModel,
+      String? cihazId,
+      int? hiz});
+}
+
+/// @nodoc
+class _$KullaniciHaritasiModelCopyWithImpl<$Res,
+        $Val extends KullaniciHaritasiModel>
+    implements $KullaniciHaritasiModelCopyWith<$Res> {
+  _$KullaniciHaritasiModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of KullaniciHaritasiModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? kullanici = freezed,
+    Object? kullaniciAdsoyad = freezed,
+    Object? tarih = freezed,
+    Object? enlem = freezed,
+    Object? boylam = freezed,
+    Object? cihazMarka = freezed,
+    Object? cihazModel = freezed,
+    Object? cihazId = freezed,
+    Object? hiz = freezed,
+  }) {
+    return _then(_value.copyWith(
+      kullanici: freezed == kullanici
+          ? _value.kullanici
+          : kullanici // ignore: cast_nullable_to_non_nullable
+              as String?,
+      kullaniciAdsoyad: freezed == kullaniciAdsoyad
+          ? _value.kullaniciAdsoyad
+          : kullaniciAdsoyad // ignore: cast_nullable_to_non_nullable
+              as String?,
+      tarih: freezed == tarih
+          ? _value.tarih
+          : tarih // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      enlem: freezed == enlem
+          ? _value.enlem
+          : enlem // ignore: cast_nullable_to_non_nullable
+              as double?,
+      boylam: freezed == boylam
+          ? _value.boylam
+          : boylam // ignore: cast_nullable_to_non_nullable
+              as double?,
+      cihazMarka: freezed == cihazMarka
+          ? _value.cihazMarka
+          : cihazMarka // ignore: cast_nullable_to_non_nullable
+              as String?,
+      cihazModel: freezed == cihazModel
+          ? _value.cihazModel
+          : cihazModel // ignore: cast_nullable_to_non_nullable
+              as String?,
+      cihazId: freezed == cihazId
+          ? _value.cihazId
+          : cihazId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      hiz: freezed == hiz
+          ? _value.hiz
+          : hiz // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$KullaniciHaritasiModelImplCopyWith<$Res>
+    implements $KullaniciHaritasiModelCopyWith<$Res> {
+  factory _$$KullaniciHaritasiModelImplCopyWith(
+          _$KullaniciHaritasiModelImpl value,
+          $Res Function(_$KullaniciHaritasiModelImpl) then) =
+      __$$KullaniciHaritasiModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? kullanici,
+      String? kullaniciAdsoyad,
+      DateTime? tarih,
+      double? enlem,
+      double? boylam,
+      String? cihazMarka,
+      String? cihazModel,
+      String? cihazId,
+      int? hiz});
+}
+
+/// @nodoc
+class __$$KullaniciHaritasiModelImplCopyWithImpl<$Res>
+    extends _$KullaniciHaritasiModelCopyWithImpl<$Res,
+        _$KullaniciHaritasiModelImpl>
+    implements _$$KullaniciHaritasiModelImplCopyWith<$Res> {
+  __$$KullaniciHaritasiModelImplCopyWithImpl(
+      _$KullaniciHaritasiModelImpl _value,
+      $Res Function(_$KullaniciHaritasiModelImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of KullaniciHaritasiModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? kullanici = freezed,
+    Object? kullaniciAdsoyad = freezed,
+    Object? tarih = freezed,
+    Object? enlem = freezed,
+    Object? boylam = freezed,
+    Object? cihazMarka = freezed,
+    Object? cihazModel = freezed,
+    Object? cihazId = freezed,
+    Object? hiz = freezed,
+  }) {
+    return _then(_$KullaniciHaritasiModelImpl(
+      kullanici: freezed == kullanici
+          ? _value.kullanici
+          : kullanici // ignore: cast_nullable_to_non_nullable
+              as String?,
+      kullaniciAdsoyad: freezed == kullaniciAdsoyad
+          ? _value.kullaniciAdsoyad
+          : kullaniciAdsoyad // ignore: cast_nullable_to_non_nullable
+              as String?,
+      tarih: freezed == tarih
+          ? _value.tarih
+          : tarih // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      enlem: freezed == enlem
+          ? _value.enlem
+          : enlem // ignore: cast_nullable_to_non_nullable
+              as double?,
+      boylam: freezed == boylam
+          ? _value.boylam
+          : boylam // ignore: cast_nullable_to_non_nullable
+              as double?,
+      cihazMarka: freezed == cihazMarka
+          ? _value.cihazMarka
+          : cihazMarka // ignore: cast_nullable_to_non_nullable
+              as String?,
+      cihazModel: freezed == cihazModel
+          ? _value.cihazModel
+          : cihazModel // ignore: cast_nullable_to_non_nullable
+              as String?,
+      cihazId: freezed == cihazId
+          ? _value.cihazId
+          : cihazId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      hiz: freezed == hiz
+          ? _value.hiz
+          : hiz // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$KullaniciHaritasiModelImpl extends _KullaniciHaritasiModel {
+  _$KullaniciHaritasiModelImpl(
+      {this.kullanici,
+      this.kullaniciAdsoyad,
+      this.tarih,
+      this.enlem,
+      this.boylam,
+      this.cihazMarka,
+      this.cihazModel,
+      this.cihazId,
+      this.hiz})
+      : super._();
+
+  factory _$KullaniciHaritasiModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$KullaniciHaritasiModelImplFromJson(json);
+
+  @override
+  String? kullanici;
+  @override
+  String? kullaniciAdsoyad;
+  @override
+  DateTime? tarih;
+  @override
+  double? enlem;
+  @override
+  double? boylam;
+  @override
+  String? cihazMarka;
+  @override
+  String? cihazModel;
+  @override
+  String? cihazId;
+  @override
+  int? hiz;
+
+  /// Create a copy of KullaniciHaritasiModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$KullaniciHaritasiModelImplCopyWith<_$KullaniciHaritasiModelImpl>
+      get copyWith => __$$KullaniciHaritasiModelImplCopyWithImpl<
+          _$KullaniciHaritasiModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$KullaniciHaritasiModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _KullaniciHaritasiModel extends KullaniciHaritasiModel {
+  factory _KullaniciHaritasiModel(
+      {String? kullanici,
+      String? kullaniciAdsoyad,
+      DateTime? tarih,
+      double? enlem,
+      double? boylam,
+      String? cihazMarka,
+      String? cihazModel,
+      String? cihazId,
+      int? hiz}) = _$KullaniciHaritasiModelImpl;
+  _KullaniciHaritasiModel._() : super._();
+
+  factory _KullaniciHaritasiModel.fromJson(Map<String, dynamic> json) =
+      _$KullaniciHaritasiModelImpl.fromJson;
+
+  @override
+  String? get kullanici;
+  set kullanici(String? value);
+  @override
+  String? get kullaniciAdsoyad;
+  set kullaniciAdsoyad(String? value);
+  @override
+  DateTime? get tarih;
+  set tarih(DateTime? value);
+  @override
+  double? get enlem;
+  set enlem(double? value);
+  @override
+  double? get boylam;
+  set boylam(double? value);
+  @override
+  String? get cihazMarka;
+  set cihazMarka(String? value);
+  @override
+  String? get cihazModel;
+  set cihazModel(String? value);
+  @override
+  String? get cihazId;
+  set cihazId(String? value);
+  @override
+  int? get hiz;
+  set hiz(int? value);
+
+  /// Create a copy of KullaniciHaritasiModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$KullaniciHaritasiModelImplCopyWith<_$KullaniciHaritasiModelImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
