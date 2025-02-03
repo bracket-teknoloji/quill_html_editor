d43eee761acb40566ad13a2d198d802029ead4c4
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-03 18:06:05 +0300

    feat: add debug menu item for Kullanıcı Haritası in menu item constants

--- lib/view/main_page/model/menu_item/menu_item_constants.dart
+++ lib/view/main_page/model/menu_item/menu_item_constants.dart
@@ -229,6 +229,7 @@ final class MenuItemConstants {
       icon: "settings",
       color: ColorPalette.tomato,
       altMenuler: [
+        if (AccountModel.instance.isDebug) GridItemModel.item(name: null, title: "Kullanıcı Haritası", route: "/kullaniciHaritasi"),
         GridItemModel.item(name: "genel_DovizKurlari", title: "Döviz Kurları", route: "/dovizKurlari"),
         GridItemModel.item(name: "ayarlar_FirmaAyarlari", title: "Firmalar", route: "/addCompany"),
         GridItemModel.item(name: "ayarlar_Oturumlar", title: "Oturumlar", route: "/oturumlar"),
