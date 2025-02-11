6adb43d9b841d7f929f2269d7d52668dc46ba667
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-11 17:57:01 +0300

    feat: implement new feature for enhanced user experience and performance optimization

--- lib/view/main_page/alt_sayfalar/cari/cari_haritasi/view/cari_haritasi_view.dart
+++ lib/view/main_page/alt_sayfalar/cari/cari_haritasi/view/cari_haritasi_view.dart
@@ -16,8 +16,8 @@ import "../../../../../../core/gen/assets.gen.dart";
 import "../../cari_listesi/model/cari_listesi_model.dart";
 import "../view_model/cari_haritasi_view_model.dart";
 
-
 typedef Konum = ({double? enlem, double? boylam});
+
 final class CariHaritasiView extends StatefulWidget {
   const CariHaritasiView({super.key, this.isGetData, this.konum, this.model});
   final bool? isGetData;
@@ -44,9 +44,6 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
   void initState() {
     // setMarker();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      if ((widget.konum?.enlem == null && widget.konum?.boylam == null) && widget.model == null) {
-        await setCameraPosition();
-      }
       final bool locationEnabled = await isLocationEnabled();
       if (!locationEnabled) {
         viewModel.setIsLocationEnabled(locationEnabled);
@@ -54,8 +51,14 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
       }
       if (widget.isGetData == null) {
         await viewModel.getData();
+      } else {
+        if (widget.model case final value?) {
+          viewModel.setCariList([value]);
+        }
       }
-
+      if ((widget.konum?.enlem != null && widget.konum?.boylam != null) || widget.model != null) {
+        await setCameraPosition();
+      }
       // markerIcon = await setMarker();
     });
     super.initState();
@@ -104,6 +107,7 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
               children: [
                 Observer(
                   builder: (_) => GoogleMap(
+                    style: googleMapsStyle(),
                     clusterManagers: {
                       ClusterManager(
                         clusterManagerId: const ClusterManagerId("value"),
@@ -121,41 +125,32 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
                     },
                     initialCameraPosition: myLocation,
                     webGestureHandling: WebGestureHandling.greedy,
+                    
                     onMapCreated: (controller) async {
-                      _controller = controller;
-                      if (widget.konum != null) {
-                        myLocation = CameraPosition(
-                          target: LatLng(widget.konum?.enlem ?? 0, widget.konum?.boylam ?? 0),
-                          zoom: 15.5,
-                        );
+                      if (_controller == null) {
+                        _controller = controller;
+                        await setCameraPosition();
                       }
-                      if (widget.model != null) {
-                        viewModel.setCariList([widget.model!]);
-                        myLocation = CameraPosition(
-                          target: LatLng(widget.model?.enlem ?? 0, widget.model?.boylam ?? 0),
-                          zoom: 15.5,
+                      for (final CariListesiModel? model in viewModel.cariList ?? []) {
+                        viewModel.addMarker(
+                          Marker(
+                            icon: await setMarker(model),
+                            clusterManagerId: const ClusterManagerId("value"),
+                            markerId: MarkerId(model?.cariKodu ?? ""),
+                            onTap: () => dialogManager.showCariIslemleriGridViewDialog(model),
+                            position: LatLng(model?.enlem ?? 0, model?.boylam ?? 0),
+                            // infoWindow: widget.isGetData == true
+                            //     ? InfoWindow.noText
+                            //     : InfoWindow(
+                            //         title: model?.cariAdi,
+                            //         snippet: model?.cariKodu,
+                            //         onTap: () => dialogManager.showCariIslemleriGridViewDialog(model),
+                            //       ),
+                          ),
                         );
-                      }
-                      if (widget.isGetData != true) {
-                        for (final CariListesiModel? model in viewModel.cariList ?? []) {
-                          viewModel.addMarker(
-                            Marker(
-                              icon: await setMarker(model),
-                              clusterManagerId: const ClusterManagerId("value"),
-                              markerId: MarkerId(model?.cariKodu ?? ""),
-                              onTap: () => dialogManager.showCariIslemleriGridViewDialog(model),
-                              position: LatLng(model?.enlem ?? 0, model?.boylam ?? 0),
-                              // infoWindow: widget.isGetData == true
-                              //     ? InfoWindow.noText
-                              //     : InfoWindow(
-                              //         title: model?.cariAdi,
-                              //         snippet: model?.cariKodu,
-                              //         onTap: () => dialogManager.showCariIslemleriGridViewDialog(model),
-                              //       ),
-                            ),
-                          );
-                          // _controller.complete(controller);
-                        }
+                        // _controller.complete(controller);
+                        // if (widget.isGetData != true) {
+                        //   }
                       }
                       setState(() {});
                     },
@@ -179,6 +174,461 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
         //   icon: const Icon(Icons.directions_boat),
         // ),
       );
+
+  String googleMapsStyle() {
+    if (theme.brightness == Brightness.dark) {
+      return """[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8ec3b9"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1a3646"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#64779e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#334e87"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6f9ba5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3C7680"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#304a7d"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2c6675"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#255763"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#b0d5ce"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3a4762"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#0e1626"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#4e6d70"
+      }
+    ]
+  }
+]
+""";
+    }
+    return """
+[
+{
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#ebe3cd"
+    }
+  ]
+},
+{
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#523735"
+    }
+  ]
+},
+{
+  "elementType": "labels.text.stroke",
+  "stylers": [
+    {
+      "color": "#f5f1e6"
+    }
+  ]
+},
+{
+  "featureType": "administrative",
+  "elementType": "geometry.stroke",
+  "stylers": [
+    {
+      "color": "#c9b2a6"
+    }
+  ]
+},
+{
+  "featureType": "administrative.land_parcel",
+  "elementType": "geometry.stroke",
+  "stylers": [
+    {
+      "color": "#dcd2be"
+    }
+  ]
+},
+{
+  "featureType": "administrative.land_parcel",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#ae9e90"
+    }
+  ]
+},
+{
+  "featureType": "landscape.natural",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+  ]
+},
+{
+  "featureType": "poi",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+  ]
+},
+{
+  "featureType": "poi",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#93817c"
+    }
+  ]
+},
+{
+  "featureType": "poi.park",
+  "elementType": "geometry.fill",
+  "stylers": [
+    {
+      "color": "#a5b076"
+    }
+  ]
+},
+{
+  "featureType": "poi.park",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#447530"
+    }
+  ]
+},
+{
+  "featureType": "road",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#f5f1e6"
+    }
+  ]
+},
+{
+  "featureType": "road.arterial",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#fdfcf8"
+    }
+  ]
+},
+{
+  "featureType": "road.highway",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#f8c967"
+    }
+  ]
+},
+{
+  "featureType": "road.highway",
+  "elementType": "geometry.stroke",
+  "stylers": [
+    {
+      "color": "#e9bc62"
+    }
+  ]
+},
+{
+  "featureType": "road.highway.controlled_access",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#e98d58"
+    }
+  ]
+},
+{
+  "featureType": "road.highway.controlled_access",
+  "elementType": "geometry.stroke",
+  "stylers": [
+    {
+      "color": "#db8555"
+    }
+  ]
+},
+{
+  "featureType": "road.local",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#806b63"
+    }
+  ]
+},
+{
+  "featureType": "transit.line",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+  ]
+},
+{
+  "featureType": "transit.line",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#8f7d77"
+    }
+  ]
+},
+{
+  "featureType": "transit.line",
+  "elementType": "labels.text.stroke",
+  "stylers": [
+    {
+      "color": "#ebe3cd"
+    }
+  ]
+},
+{
+  "featureType": "transit.station",
+  "elementType": "geometry",
+  "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+  ]
+},
+{
+  "featureType": "water",
+  "elementType": "geometry.fill",
+  "stylers": [
+    {
+      "color": "#b9d3c2"
+    }
+  ]
+},
+{
+  "featureType": "water",
+  "elementType": "labels.text.fill",
+  "stylers": [
+    {
+      "color": "#92998d"
+    }
+  ]
+}
+]""";
+  }
 
   Future<BitmapDescriptor> setMarker(CariListesiModel? model) => Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -206,11 +656,23 @@ final class CariHaritasiViewState extends BaseState<CariHaritasiView> {
 
   Future<void> setCameraPosition() async {
     try {
-      final location = await _locationTracker.getLocation();
-      myLocation = CameraPosition(
-        target: LatLng(location.latitude ?? 0, location.longitude ?? 0),
-        zoom: 15.5,
-      );
+      if (widget.konum case final value?) {
+        myLocation = CameraPosition(
+          target: LatLng(value.enlem ?? 0, value.boylam ?? 0),
+          zoom: 15.5,
+        );
+      } else if (widget.model case final value?) {
+        myLocation = CameraPosition(
+          target: LatLng(value.enlem ?? 0, value.boylam ?? 0),
+          zoom: 15.5,
+        );
+      } else {
+        final location = await _locationTracker.getLocation();
+        myLocation = CameraPosition(
+          target: LatLng(location.latitude ?? 0, location.longitude ?? 0),
+          zoom: 15.5,
+        );
+      }
       _controller?.animateCamera(CameraUpdate.newCameraPosition(myLocation));
     } catch (e) {
       dialogManager.showAlertDialog(e.toString());
