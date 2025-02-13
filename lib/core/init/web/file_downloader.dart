b6e805a03d88f88595e7044e468719eb101b0ced
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-13 09:09:21 +0300

    feat: update file downloader to use js_interop for improved web compatibility

--- lib/core/init/web/file_downloader.dart
+++ lib/core/init/web/file_downloader.dart
@@ -1,14 +1,20 @@
-import "dart:html" as web_file;
+import "dart:js_interop" as js;
 
 import "package:flutter/foundation.dart";
+import "package:web/web.dart" as web_file;
 
 void fileDownload(Uint8List data, String fileName) {
   if (kIsWeb) {
-    final blob = web_file.Blob([data], "application/pdf", "native");
+    final blob = web_file.Blob(
+      js.JSArray()..add(data.jsify()),
+      web_file.BlobPropertyBag(
+        type: "native",
+        endings: "application/pdf",
+      ),
+    );
 
-    web_file.AnchorElement(
-      href: web_file.Url.createObjectUrlFromBlob(blob),
-    )
+    web_file.HTMLAnchorElement()
+      ..href = web_file.URL.createObjectURL(blob)
       ..setAttribute("download", fileName)
       ..click();
   }
