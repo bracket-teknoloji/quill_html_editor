7a90908a1f4f55597a4bdc1cc3159e366796d0b9
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-14 08:55:20 +0300

    chore: update version codes and script references across platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         applicationId = "com.bracket23.picker"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 319
+        versionCode = 320
         versionName = flutterVersionName
     }
 
