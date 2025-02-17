d62d9d421f8626533253d3f510c6121cc701cecf
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-17 12:08:51 +0300

    refactor: enhance PaketIcerigiCard to utilize SeriHareketleriRequestModel for improved stock handling

--- lib/core/components/card/paket_icerigi_card.dart
+++ lib/core/components/card/paket_icerigi_card.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:get/get.dart";
+import "package:picker/core/base/view/seri_islemleri/seri_hareketleri/model/seri_hareketleri_request_model.dart";
 
 import "../../../view/main_page/alt_sayfalar/stok/paketleme/paket_icerigi/model/paket_icerigi_model.dart";
 import "../../../view/main_page/alt_sayfalar/stok/stok_liste/model/stok_listesi_model.dart";
@@ -27,64 +28,60 @@ final class _PaketIcerigiCardState extends BaseState<PaketIcerigiCard> {
 
   @override
   Widget build(BuildContext context) => Card(
-        child: ListTile(
-          onTap: onTap,
-          title: Text(item.stokAdi ?? ""),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    child: ListTile(
+      onTap: onTap,
+      title: Text(item.stokAdi ?? ""),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [if (item.cikistaSeri == "E") const ColorfulBadge(label: Text("Serili"), badgeColorEnum: BadgeColorEnum.seri)]),
+          CustomLayoutBuilder(
+            splitCount: 2,
             children: [
-              Row(
-                children: [
-                  if (item.cikistaSeri == "E") const ColorfulBadge(label: Text("Serili"), badgeColorEnum: BadgeColorEnum.seri),
-                ],
-              ),
-              CustomLayoutBuilder(
-                splitCount: 2,
-                children: [
-                  Text("Stok Kodu: ${item.stokKodu ?? ""}"),
-                  Text("Miktar: ${item.miktar.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)}"),
-                  if (item.cikistaSeri == "E") Text("Seriler: ${item.seriList?.length ?? ""} adet seri"),
-                ],
-              ),
-              Text(item.seriList?.map((e) => e.seriNo).join(", ") ?? ""),
+              Text("Stok Kodu: ${item.stokKodu ?? ""}"),
+              Text("Miktar: ${item.miktar.commaSeparatedWithDecimalDigits(OndalikEnum.miktar)}"),
+              if (item.cikistaSeri == "E") Text("Seriler: ${item.seriList?.length ?? ""} adet seri"),
             ],
           ),
-        ),
-      );
+          Text(item.seriList?.map((e) => e.seriNo).join(", ") ?? ""),
+        ],
+      ),
+    ),
+  );
 
   void onTap() => bottomSheetDialogManager.showBottomSheetDialog(
-        context,
-        title: item.stokKodu ?? "",
-        children: [
-          if (yetkiController.stokPaketlemeSil && !widget.kilitliMi)
-            BottomSheetModel(
-              title: loc.generalStrings.delete,
-              iconWidget: Icons.delete_outline_outlined,
-              onTap: () async {
-                widget.onDeleted();
-              },
-            ),
-          if (yetkiController.seriIslemleri && yetkiController.seriUygulamasiAcikMi)
-            BottomSheetModel(
-              title: "Seri Listesi",
-              iconWidget: Icons.dynamic_form_outlined,
-              onTap: () async {
-                Get
-                  ..back()
-                  ..toNamed("/seriHareketleri", arguments: await getStok);
-              },
-            ),
-          if (yetkiController.stokListesi)
-            BottomSheetModel(
-              title: "Stok İşlemleri",
-              iconWidget: Icons.list_alt_outlined,
-              onTap: () async {
-                Get.back();
-                dialogManager.showStokGridViewDialog(await getStok);
-              },
-            ),
-        ],
-      );
+    context,
+    title: item.stokKodu ?? "",
+    children: [
+      if (yetkiController.stokPaketlemeSil && !widget.kilitliMi)
+        BottomSheetModel(
+          title: loc.generalStrings.delete,
+          iconWidget: Icons.delete_outline_outlined,
+          onTap: () async {
+            widget.onDeleted();
+          },
+        ),
+      if (yetkiController.seriIslemleri && yetkiController.seriUygulamasiAcikMi)
+        BottomSheetModel(
+          title: "Seri Listesi",
+          iconWidget: Icons.dynamic_form_outlined,
+          onTap: () async {
+            Get
+              ..back()
+              ..toNamed("/seriHareketleri", arguments: SeriHareketleriRequestModel(stokKodu: (await getStok)?.stokKodu, hareketId: item.id));
+          },
+        ),
+      if (yetkiController.stokListesi)
+        BottomSheetModel(
+          title: "Stok İşlemleri",
+          iconWidget: Icons.list_alt_outlined,
+          onTap: () async {
+            Get.back();
+            dialogManager.showStokGridViewDialog(await getStok);
+          },
+        ),
+    ],
+  );
 
   Future<StokListesiModel?> get getStok async => await networkManager.getStokModel(StokRehberiRequestModel(stokKodu: item.stokKodu));
 }
