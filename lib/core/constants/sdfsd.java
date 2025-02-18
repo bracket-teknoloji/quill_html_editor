d4293ba8170e2749bafdff8a2a0c543814cfd0bd
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-18 17:53:56 +0300

    chore: update flutter.js version in index.html and clean up sdfsd.java for improved readability

--- lib/core/constants/sdfsd.java
+++ lib/core/constants/sdfsd.java
@@ -1,43 +1,29 @@
 if (fiyatRehberGorebilir()) {
-   if (!_paramModel.OzelFiyatSistemi) { //apk 166 : if (_paramModel.OzelFiyatSistemi == false) eklendi
-    if (_initStokResponseModel.dovizliMi(_satisaYonelikMi)) {
-     b.layoutDovizFiyati.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
-      @Override
-      public void onOneClick(View v) {
-       b.layoutBrutFiyatTL.setRightImageResource(0);
-       showFiyatSecDialog(b.layoutDovizFiyati);
-      }
-     });
-    } else {
-     b.layoutBrutFiyatTL.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
-      @Override
-      public void onOneClick(View v) {
-       b.layoutDovizFiyati.setRightImageResource(0);
-       showFiyatSecDialog(b.layoutBrutFiyatTL);
-      }
-     });
-    }
-   } else { // APK 233
-    b.layoutBrutFiyatTL.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
-     @Override
-     public void onOneClick(View v) {
-      b.layoutDovizFiyati.setRightImageResource(0);
-      showListeFiyatSecDialog(b.layoutBrutFiyatTL);
-     }
-    });
-   }
-  }
-private boolean fiyatRehberGorebilir() {
-  if (_profilModel.kullanici_AdminMi) return true;
-  if (!_profilModel.stok_FiyatGorEkrani) return false;
-
-  if (_alisaYonelikMi) {
-   return (_profilModel.stokFiyatGorEkraniGorunecekAlanMi("alis_doviz_fiyati") || _profilModel.stokFiyatGorEkraniGorunecekAlanMi(
-     "alis_fiyat1")  _profilModel.stokFiyatGorEkraniGorunecekAlanMi("alis_fiyat2")  _profilModel.stokFiyatGorEkraniGorunecekAlanMi(
-     "alis_fiyat3") || _profilModel.stokFiyatGorEkraniGorunecekAlanMi("alis_fiyat4"));
-  } else {
-   return (_profilModel.stokFiyatGorEkraniGorunecekAlanMi("satis_doviz_fiyati") || _profilModel.stokFiyatGorEkraniGorunecekAlanMi(
-     "satis_fiyat1")  _profilModel.stokFiyatGorEkraniGorunecekAlanMi("satis_fiyat2")  _profilModel.stokFiyatGorEkraniGorunecekAlanMi(
-     "satis_fiyat3") || _profilModel.stokFiyatGorEkraniGorunecekAlanMi("satis_fiyat4"));
-  }
- }
+			if (!_paramModel.OzelFiyatSistemi) {
+				if (_initStokResponseModel.dovizliMi(_satisaYonelikMi)) {
+					b.layoutDovizFiyati.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
+						@Override
+						public void onOneClick(View v) {
+							b.layoutBrutFiyatTL.setRightImageResource(0);
+							showFiyatSecDialog(b.layoutDovizFiyati);
+						}
+					});
+				} else {
+					b.layoutBrutFiyatTL.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
+						@Override
+						public void onOneClick(View v) {
+							b.layoutDovizFiyati.setRightImageResource(0);
+							showFiyatSecDialog(b.layoutBrutFiyatTL);
+						}
+					});
+				}
+			} else { // APK 233
+				b.layoutBrutFiyatTL.setRightImageResource(R.drawable.ic_rehber_gray, new OneClickListener() {
+					@Override
+					public void onOneClick(View v) {
+						b.layoutDovizFiyati.setRightImageResource(0);
+						showListeFiyatSecDialog(b.layoutBrutFiyatTL);
+					}
+				});
+			}
+		}
