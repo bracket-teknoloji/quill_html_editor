d72e9b087819741c19233acd668cd64c16ab1652
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-02-19 20:16:59 +0300

    chore: update version codes and script references for build consistency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         applicationId = "com.bracket23.picker"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 325
+        versionCode = 326
         versionName = flutterVersionName
     }
 
