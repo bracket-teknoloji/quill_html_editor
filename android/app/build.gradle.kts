dfb40bbbcf44ac3d402a78f980fceebb5b6b995a
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-03-05 15:01:07 +0300

    feat: update dependencies and increment version codes for iOS and Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         applicationId = "com.bracket23.picker"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 339
+        versionCode = 342
         versionName = flutterVersionName
     }
 
