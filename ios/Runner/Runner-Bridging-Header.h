fa2e451496767ff4ecb742d1d555a96e32a5310b
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-03-17 17:02:28 +0300

    feat: add Sewoo bridging header and update Info.plist for enhanced printer support

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,2 @@
 #import "GeneratedPluginRegistrant.h"
+#import "Sewoo_Bridging_Header.h"
--- ios/Runner/Sewoo_Bridging_Header.h
+++ ios/Runner/Sewoo_Bridging_Header.h
@@ -8,5 +8,11 @@
 #ifndef Sewoo_Bridging_Header_h
 #define Sewoo_Bridging_Header_h
 
+#import "CallbackData.h"
+#import "EABluetoothPort.h"
+#import "EPLPrinter.h"
+#import "PortDirect.h"
+#import "PortInterface.h"
+#import "WiFiPortInterface.h"
 
 #endif /* Sewoo_Bridging_Header_h */
