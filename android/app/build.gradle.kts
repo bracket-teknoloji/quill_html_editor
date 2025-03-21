de53a0d961113b046001a7780b4951a51d3a82a0
Burak Talha Yılmaz <buraktalha28.by@gmail.com>
2025-03-21 09:44:44 +0300

    chore: update version code to 358 and improve Fastfile output paths

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         applicationId = "com.bracket23.picker"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 353
+        versionCode = 358
         versionName = flutterVersionName
     }
 
